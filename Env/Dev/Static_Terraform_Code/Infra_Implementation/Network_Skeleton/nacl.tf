33f68dbe4f409081cb1df10ec7a5c736a84194fd
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-20 13:02:57 +0530

    Update nacl.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nacl.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nacl.tf
@@ -160,7 +160,9 @@ resource "aws_network_acl_association" "database" {
   network_acl_id = aws_network_acl.database.id
 }
 
-
+############################################
+# Public
+############################################
 # Inbound
 
 resource "aws_network_acl_rule" "public_http" {
@@ -273,4 +275,309 @@ resource "aws_network_acl_rule" "public_ephemeral" {
   cidr_block     = "0.0.0.0/0"
   from_port      = 1024
   to_port        = 65535
+}
+
+
+############################################
+# Frontend
+############################################
+
+resource "aws_network_acl_rule" "frontend_alb" {
+  network_acl_id = aws_network_acl.frontend.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 3000
+  to_port   = 3000
+}
+
+resource "aws_network_acl_rule" "frontend_ephemeral" {
+  network_acl_id = aws_network_acl.frontend.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 1024
+  to_port   = 65535
+}
+
+resource "aws_network_acl_rule" "frontend_ssm" {
+  network_acl_id = aws_network_acl.frontend.id
+  rule_number    = 120
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
+}
+
+# outbound
+
+resource "aws_network_acl_rule" "frontend_return" {
+  network_acl_id = aws_network_acl.frontend.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 1024
+  to_port   = 65535
+}
+
+resource "aws_network_acl_rule" "frontend_https" {
+  network_acl_id = aws_network_acl.frontend.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
+}
+
+############################################
+# Backend
+############################################
+
+resource "aws_network_acl_rule" "backend_employee" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 8080
+  to_port   = 8080
+}
+
+resource "aws_network_acl_rule" "backend_attendance" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 8081
+  to_port   = 8081
+}
+
+resource "aws_network_acl_rule" "backend_salary" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 120
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 8082
+  to_port   = 8082
+}
+
+resource "aws_network_acl_rule" "backend_notification" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 130
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 8085
+  to_port   = 8085
+}
+
+resource "aws_network_acl_rule" "backend_ephemeral" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 140
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 1024
+  to_port   = 65535
+}
+
+resource "aws_network_acl_rule" "backend_ssm" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 150
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
+}
+
+# Outbound
+
+resource "aws_network_acl_rule" "backend_redis" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.96/27"
+
+  from_port = 6379
+  to_port   = 6379
+}
+
+resource "aws_network_acl_rule" "backend_postgres" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.96/27"
+
+  from_port = 5432
+  to_port   = 5432
+}
+
+resource "aws_network_acl_rule" "backend_scylla" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 120
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.96/27"
+
+  from_port = 9042
+  to_port   = 9042
+}
+
+resource "aws_network_acl_rule" "backend_return" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 130
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.0/27"
+
+  from_port = 1024
+  to_port   = 65535
+}
+
+resource "aws_network_acl_rule" "backend_https" {
+  network_acl_id = aws_network_acl.backend.id
+  rule_number    = 140
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
+}
+
+############################################
+# DataBase
+############################################
+# Inbound
+resource "aws_network_acl_rule" "database_redis" {
+  network_acl_id = aws_network_acl.database.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.64/27"
+
+  from_port = 6379
+  to_port   = 6379
+}
+
+resource "aws_network_acl_rule" "database_postgres" {
+  network_acl_id = aws_network_acl.database.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.64/27"
+
+  from_port = 5432
+  to_port   = 5432
+}
+
+resource "aws_network_acl_rule" "database_scylla" {
+  network_acl_id = aws_network_acl.database.id
+  rule_number    = 120
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.64/27"
+
+  from_port = 9042
+  to_port   = 9042
+}
+
+resource "aws_network_acl_rule" "database_ssm" {
+  network_acl_id = aws_network_acl.database.id
+  rule_number    = 130
+  egress         = false
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
+}
+
+# Outbound
+
+resource "aws_network_acl_rule" "database_return" {
+  network_acl_id = aws_network_acl.database.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "10.0.0.64/27"
+
+  from_port = 1024
+  to_port   = 65535
+}
+
+resource "aws_network_acl_rule" "database_https" {
+  network_acl_id = aws_network_acl.database.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "6"
+  rule_action    = "allow"
+
+  cidr_block = "0.0.0.0/0"
+
+  from_port = 443
+  to_port   = 443
 }
