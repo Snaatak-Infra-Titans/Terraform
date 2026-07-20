c576ab2d5bf04d3d81eed50761dd1dc5630ab2b0
VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>
2026-07-20 23:03:12 +0530

    Delete Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/outputs.tf
    Signed-off-by: VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = aws_vpc.main_vpc.id
-}
-
-output "vpc_cidr" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main_vpc.cidr_block
-}
