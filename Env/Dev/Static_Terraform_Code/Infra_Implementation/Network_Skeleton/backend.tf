7552a6ac30268d19bfb772405cc53203865a0c88
Pawan2031 <pawan.tiwari.snaatak@mygurukulam.co>
2026-07-21 00:05:25 +0530

    Update backend.tf
    Signed-off-by: Pawan2031 <pawan.tiwari.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
-    key          = "network/vpc.tfstate"
+    key          = "network/subnets.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
