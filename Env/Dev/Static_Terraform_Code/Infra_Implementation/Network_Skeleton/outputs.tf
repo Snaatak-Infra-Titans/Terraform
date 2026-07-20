6c29c4d78167f0997732d3c65fdd1921f6611aa6
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-20 12:28:00 +0530

    Update outputs.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/outputs.tf
@@ -7,3 +7,14 @@ output "vpc_cidr" {
   description = "CIDR block of the VPC"
   value       = aws_vpc.main_vpc.cidr_block
 }
+
+output "key_pair_name" {
+  description = "Name of the SSH Key Pair"
+  value       = aws_key_pair.main_key.key_name
+}
+
+output "private_key_pem" {
+  description = "Private key content"
+  value       = tls_private_key.rsa_key.private_key_pem
+  sensitive   = true
+}
