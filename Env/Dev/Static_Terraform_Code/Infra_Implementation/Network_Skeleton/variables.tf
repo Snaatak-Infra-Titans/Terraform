b4eb1642ddea3721170fba2d1f88c8b104aa799d
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-20 11:46:32 +0530

    Update variables.tf
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/variables.tf
@@ -27,21 +27,3 @@ variable "vpc_name" {
   description = "Name tag of the existing VPC"
   type        = string
 }
-
-variable "vpc_id" {
-  description = "Existing VPC ID. Use this if you want exact lookup"
-  type        = string
-  default     = ""
-}
-
-variable "subnet_newbits" {
-  description = "Number of bits to add for subnetting"
-  type        = number
-  default     = 3
-}
-
-variable "azs" {
-  description = "Availability zones suffixes to use"
-  type        = list(string)
-  default     = ["a", "b"]
-}
