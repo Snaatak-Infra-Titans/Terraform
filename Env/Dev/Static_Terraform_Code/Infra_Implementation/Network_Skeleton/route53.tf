c30151055cecc639c095369ce2b365f47639c353
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-20 15:14:44 +0530

    Update route53.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53.tf
@@ -1,18 +1,44 @@
-# Lookup the existing public Route 53 hosted zone
-data "aws_route53_zone" "selected" {
-  name         = var.hosted_zone_name
-  private_zone = false
+
+# -----------------------------------------------------
+# Fetch the existing Application Load Balancer
+# -----------------------------------------------------
+
+data "aws_lb" "existing" {
+  name = var.alb_name
 }
 
-# Create www.otms.online alias record pointing to the ALB
+# -----------------------------------------------------
+# Create public Route 53 hosted zone
+# Hosted zone: otms.online
+# -----------------------------------------------------
+
+resource "aws_route53_zone" "main" {
+  name    = var.hosted_zone_name
+  comment = "Public hosted zone for ${var.application}-${var.environment}"
+
+  tags = {
+    Name        = "${var.environment}-${var.application}-hosted-zone"
+    Environment = var.environment
+    Application = var.application
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+  }
+}
+
+# -----------------------------------------------------
+# Create www.otms.online alias record
+# Point it to the existing ALB
+# -----------------------------------------------------
+
 resource "aws_route53_record" "alb_alias" {
-  zone_id = data.aws_route53_zone.selected.zone_id
+  zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
   type    = "A"
 
   alias {
-    name                   = aws_lb.this.dns_name
-    zone_id                = aws_lb.this.zone_id
+    name                   = data.aws_lb.existing.dns_name
+    zone_id                = data.aws_lb.existing.zone_id
     evaluate_target_health = true
   }
 }
+
