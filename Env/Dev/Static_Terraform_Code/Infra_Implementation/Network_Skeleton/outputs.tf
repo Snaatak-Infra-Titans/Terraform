2f7b42d4f8b32edfc5e0803a1b7ba52b8b88e54a
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-20 12:44:58 +0530

    Update outputs.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/outputs.tf
@@ -1,13 +1,3 @@
-output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = aws_vpc.main_vpc.id
-}
-
-output "vpc_cidr" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main_vpc.cidr_block
-}
-
 output "key_pair_name" {
   description = "Name of the SSH Key Pair"
   value       = aws_key_pair.main_key.key_name
