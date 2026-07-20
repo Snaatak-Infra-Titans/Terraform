3f0da3acc90994a9cf7d55997449705f78917fe3
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-20 12:33:22 +0530

    deletedoutputs.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nat_gw.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nat_gw.tf
@@ -1,6 +1,53 @@
-# -----------------------------------------------------------
-# Elastic IP for NAT Gateway
-# -----------------------------------------------------------
+############################################################
+# Existing VPC
+############################################################
+data "aws_vpc" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+############################################################
+# Existing Internet Gateway
+############################################################
+data "aws_internet_gateway" "existing" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
+
+############################################################
+# Existing Public Subnets
+############################################################
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+
+  filter {
+    name   = "tag:Tier"
+    values = ["public"]
+  }
+}
+
+############################################################
+# Existing Private Route Table
+############################################################
+data "aws_route_table" "private" {
+  vpc_id = data.aws_vpc.existing.id
+
+  filter {
+    name   = "tag:Tier"
+    values = ["private"]
+  }
+}
+
+############################################################
+# Elastic IP
+############################################################
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
 
@@ -13,14 +60,12 @@ resource "aws_eip" "nat_eip" {
   }
 }
 
-# -----------------------------------------------------------
+############################################################
 # NAT Gateway
-# -----------------------------------------------------------
+############################################################
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-
-  # Place NAT Gateway in one Public Subnet
-  subnet_id = data.aws_subnets.public.ids[0]
+  subnet_id     = data.aws_subnets.public.ids[0]
 
   tags = {
     Name        = "${var.environment}_${var.application}_nat_gw"
@@ -30,17 +75,14 @@ resource "aws_nat_gateway" "nat" {
     CostCenter  = var.cost_center
   }
 
-  depends_on = [
-    data.aws_internet_gateway.existing_igw
-  ]
+  depends_on = [data.aws_internet_gateway.existing]
 }
 
-# -----------------------------------------------------------
-# Private Route
-# -----------------------------------------------------------
+############################################################
+# Default Route
+############################################################
 resource "aws_route" "private_nat_route" {
-  route_table_id         = aws_route_table.private.id
+  route_table_id         = data.aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-
-  nat_gateway_id = aws_nat_gateway.nat.id
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
