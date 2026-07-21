a146db2bc0bfa352e99ea7892894db99b5421516
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-21 20:02:30 +0530

    Create target_gp.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/target_gp.tf
@@ -0,0 +1,21 @@
+resource "aws_lb_target_group" "notification_tg" {
+  name     = "${var.environment}-${var.application}-notification-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.network_vpc.id
+
+  # Health Check ensure karega ki traffic sirf healthy instances par jaye
+  health_check {
+    path                = "/"            # Ise apne app ke hisaab se update kar lena
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 4
+    interval            = 15
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.application}-notification-tg"
+  }
+}
