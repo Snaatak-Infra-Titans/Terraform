1fcc0c8ca3b4432cd1470155dcba426b3e4c851b
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-21 20:31:11 +0530

    Create listn_rule.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/listn_rule.tf
@@ -0,0 +1,22 @@
+listn_rule.tf
+
+
+resource "aws_lb_listener_rule" "notification_routing" {
+  listener_arn = data.aws_lb_listener.app_listener.arn
+  priority     = var.listener_rule_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.notification_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = [var.app_path_pattern]
+    }
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.application}-listener-rule"
+  }
+}
