92b1808de9f98ce77e4ef13dee0a62931f885845
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-21 19:39:41 +0530

    Create security group for notification application
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/sg.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "notification_sg" {
+  name        = "${var.environment}-${var.application}-notification-sg"
+  description = "Security group for notification backend instances"
+  vpc_id      = data.aws_vpc.network_vpc.id
+
+  ingress {
+    description     = "Allow traffic from ALB"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [data.aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    description     = "Allow SSH from Bastion"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [data.aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.application}-notification-sg"
+  }
+}
