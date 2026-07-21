9e2114090e361fbcacbda2e2d1dcc5840c6f7f97
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-21 17:33:24 +0530

    Update route53.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53.tf
@@ -15,15 +15,29 @@ resource "aws_route53_zone" "main" {
 }
 
 # -----------------------------------------------------
-# Create Alias Record pointing to Versha's ALB
+# 1. Alias Record for www.otms.online (Subdomain)
 # -----------------------------------------------------
-resource "aws_route53_record" "alb_alias" {
+resource "aws_route53_record" "alb_alias_www" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = var.domain_name
+  name    = var.domain_name  # "www.otms.online"
   type    = "A"
 
   alias {
-    # DIRECT LINK TO VERSHA'S ALB
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
+}
+
+# -----------------------------------------------------
+# 2. Alias Record for otms.online (Root / Apex Domain)
+# -----------------------------------------------------
+resource "aws_route53_record" "alb_alias_root" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.hosted_zone_name  # "otms.online" (Leave empty string "" or use hosted_zone_name)
+  type    = "A"
+
+  alias {
     name                   = aws_lb.this.dns_name
     zone_id                = aws_lb.this.zone_id
     evaluate_target_health = true
