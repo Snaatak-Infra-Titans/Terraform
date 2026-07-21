f69957130a2bf7b7eed4492018830a950498cf11
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-21 17:30:10 +0530

    Update route53.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53.tf
@@ -1,17 +1,6 @@
-
-# -----------------------------------------------------
-# Fetch the existing Application Load Balancer
-# -----------------------------------------------------
-
-data "aws_lb" "existing" {
-  name = var.alb_name
-}
-
 # -----------------------------------------------------
 # Create public Route 53 hosted zone
-# Hosted zone: otms.online
 # -----------------------------------------------------
-
 resource "aws_route53_zone" "main" {
   name    = var.hosted_zone_name
   comment = "Public hosted zone for ${var.application}-${var.environment}"
@@ -26,19 +15,17 @@ resource "aws_route53_zone" "main" {
 }
 
 # -----------------------------------------------------
-# Create www.otms.online alias record
-# Point it to the existing ALB
+# Create Alias Record pointing to Versha's ALB
 # -----------------------------------------------------
-
 resource "aws_route53_record" "alb_alias" {
   zone_id = aws_route53_zone.main.zone_id
   name    = var.domain_name
   type    = "A"
 
   alias {
-    name                   = data.aws_lb.existing.dns_name
-    zone_id                = data.aws_lb.existing.zone_id
+    # DIRECT LINK TO VERSHA'S ALB
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
     evaluate_target_health = true
   }
 }
-
