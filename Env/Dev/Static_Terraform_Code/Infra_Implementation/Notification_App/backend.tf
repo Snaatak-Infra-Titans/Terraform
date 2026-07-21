728148f50e57b2652f199fc09fe4f020906661da
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-21 14:05:11 +0530

    Update backend.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
-    key          = "dev/apps/notification/terraform.tfstate"
+    key          = "notification/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
