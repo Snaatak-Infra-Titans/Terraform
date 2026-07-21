cacdc988afed332e935f676f2141c08f9e280bd3
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-21 20:07:32 +0530

    Add AWS Auto Scaling Group for notification app
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
@@ -0,0 +1,28 @@
+resource "aws_autoscaling_group" "notification_asg" {
+  name_prefix         = "${var.environment}-${var.application}-notification-asg-"
+  vpc_zone_identifier = data.aws_subnets.backend_subnets.ids
+
+  # Yahan hum 408 wale Target Group ko direct link kar rahe hain
+  target_group_arns   = [aws_lb_target_group.notification_tg.arn]
+
+  desired_capacity          = var.asg_desired_capacity
+  min_size                  = var.asg_min_size
+  max_size                  = var.asg_max_size
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.notification_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.environment}-${var.application}-asg-instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
