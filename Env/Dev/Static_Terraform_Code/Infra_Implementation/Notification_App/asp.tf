224258ab035c62a5628b1694523f4fceef9ff89e
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-21 20:19:30 +0530

    Add CPU target tracking autoscaling policy
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asp.tf
@@ -0,0 +1,15 @@
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "${var.environment}-${var.application}-cpu-policy"
+  
+  autoscaling_group_name = aws_autoscaling_group.notification_asg.name
+  
+  policy_type            = "TargetTrackingScaling"
+  
+  # Target Tracking Configuration (CPU based)
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = var.cpu_target_value
+  }
+}
