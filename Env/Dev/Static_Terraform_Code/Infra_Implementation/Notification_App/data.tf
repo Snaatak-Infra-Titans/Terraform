bb03854c8809e19848c7c401cc2c0427413a0c09
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-21 20:25:04 +0530

    Update data.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/data.tf
@@ -47,3 +47,13 @@ data "aws_security_group" "bastion_sg" {
     values = ["*bastion*"]
   }
 }
+
+data "aws_lb" "existing_alb" {
+  # Apne actual Shared ALB ka naam yahan verify karke daalna
+  name = "${var.environment}-shared-alb"
+}
+
+data "aws_lb_listener" "app_listener" {
+  load_balancer_arn = data.aws_lb.existing_alb.arn
+  port              = 80 # Agar HTTPS use kar rahe ho toh isko 443 kar dena
+}
