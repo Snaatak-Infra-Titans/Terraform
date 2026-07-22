5d98a282434e89bffff45d3250b2dae29b147e1a
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-22 23:10:06 +0530

    Update launch_tplate.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/launch_tplate.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/launch_tplate.tf
@@ -1,17 +1,26 @@
 resource "aws_launch_template" "notification_lt" {
-  name_prefix   = "${var.environment}-${var.application}-notification-lt-"
-
+  name          = "${var.environment}-${var.application}-notification-lt"
   image_id      = data.aws_ami.notification_app.id
-
   instance_type = var.instance_type
   key_name      = data.aws_key_pair.existing_key.key_name
 
-  vpc_security_group_ids = [aws_security_group.notification_sg.id]
+  vpc_security_group_ids = [
+    aws_security_group.notification_sg.id
+  ]
+
+  iam_instance_profile {
+    name = var.ssm_instance_profile
+  }
 
   tag_specifications {
     resource_type = "instance"
+
     tags = {
-      Name = "${var.environment}-${var.application}-notification-app"
+      Name        = "${var.environment}-${var.application}-notification"
+      Environment = var.environment
+      Application = var.application
+      Owner       = var.owner
+      CostCenter  = var.cost_center
     }
   }
 
