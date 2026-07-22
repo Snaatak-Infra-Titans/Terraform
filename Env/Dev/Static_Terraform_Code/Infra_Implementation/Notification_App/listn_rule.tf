1b4062e02269bc014f54d3bda26903bb727facc7
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-22 07:49:20 +0530

    Update listn_rule.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/listn_rule.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/listn_rule.tf
@@ -1,19 +1,5 @@
-resource "aws_lb_listener_rule" "notification_routing" {
+# Fetching the Listener Rule created by the Network Skeleton
+data "aws_lb_listener_rule" "notification_routing" {
   listener_arn = data.aws_lb_listener.app_listener.arn
-  priority     = var.listener_rule_priority
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.notification_tg.arn
-  }
-
-  condition {
-    path_pattern {
-      values = [var.app_path_pattern]
-    }
-  }
-
-  tags = {
-    Name = "${var.environment}-${var.application}-listener-rule"
-  }
+  priority     = 40
 }
