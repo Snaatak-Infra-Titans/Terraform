ce38de765300bc9d5179b59c1220770c9aded45e
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-22 08:11:18 +0530

    Update data.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/data.tf
@@ -15,9 +15,32 @@ data "aws_subnets" "backend_subnets" {
   }
 }
 
-data "aws_lb_target_group" "notification_tg" {
-  name = "${var.environment}-${var.application}-notification-tg"
+# Fetching the existing ALB from Network Skeleton
+data "aws_lb" "existing_alb" {
+  name = "${var.environment}-${var.application}-alb"
 }
+
+# Fetching the HTTPS Listener (Port 443) for routing rules
+data "aws_lb_listener" "app_listener" {
+  load_balancer_arn = data.aws_lb.existing_alb.arn
+  port              = 443
+}
+
+data "aws_security_group" "alb_sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["*alb*"]
+  }
+}
+
+/*
+data "aws_security_group" "bastion_sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["*bastion*"]
+  }
+}
+*/
 
 data "aws_key_pair" "existing_key" {
   key_name = var.key_name
@@ -30,19 +53,5 @@ data "aws_ami" "notification_app" {
   filter {
     name   = "name"
     values = [var.ami_name]
-  }
-}
-
-data "aws_security_group" "alb_sg" {
-  filter {
-    name   = "tag:Name"
-    values = ["*alb*"]
-  }
-}
-
-data "aws_security_group" "bastion_sg" {
-  filter {
-    name   = "tag:Name"
-    values = ["*bastion*"]
   }
 }
