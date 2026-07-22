00361268d8595db24c5c2fa94be3b24d9d757fef
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-22 21:28:20 +0530

    Update launch_tplate.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/launch_tplate.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/launch_tplate.tf
@@ -12,28 +12,6 @@ resource "aws_launch_template" "notification_lt" {
     name = var.ssm_instance_profile
   }
 
-/*
-user_data = base64encode(<<-EOF
-#!/bin/bash
-set -euxo pipefail
-
-# Wait for networking
-sleep 20
-
-# Remove stale SSM registration from the AMI
-systemctl stop snap.amazon-ssm-agent.amazon-ssm-agent || true
-rm -rf /var/lib/amazon/ssm/*
-systemctl start snap.amazon-ssm-agent.amazon-ssm-agent
-
-# Configure MTU
-NIC=$(ip route | awk '/default/ {print $5}')
-ip link set dev "$NIC" mtu 1400 || true
-
-# Restart Notification API
-systemctl restart notification-api || true
-EOF
-  )
-*/
   tag_specifications {
     resource_type = "instance"
 
