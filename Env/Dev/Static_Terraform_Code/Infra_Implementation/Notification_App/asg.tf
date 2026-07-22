ff1ff5c252552dc39f3e491dba172855d0e6749e
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-22 22:32:32 +0530

    Update asg.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_group" "notification_asg" {
   name_prefix         = "${var.environment}-${var.application}-notification-asg-"
   vpc_zone_identifier = data.aws_subnets.backend_subnets.ids
 
-  target_group_arns   = [aws_lb_target_group.notification_tg.arn]
+  target_group_arns   = [data.aws_lb_target_group.notification_tg.arn]
 
   desired_capacity          = var.asg_desired_capacity
   min_size                  = var.asg_min_size
