d81fc26d0fc07ffdc0f14a3fbd7de209dd225e77
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-22 23:22:30 +0530

    Update asg.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/asg.tf
@@ -1,23 +1,24 @@
 resource "aws_autoscaling_group" "notification_asg" {
-  name                = "${var.environment}-${var.application}-notification-asg-"
+  name                = "${var.environment}-${var.application}-notification-asg"
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
+  desired_capacity    = var.asg_desired_capacity
   vpc_zone_identifier = data.aws_subnets.backend_subnets.ids
 
-  target_group_arns   = [data.aws_lb_target_group.notification_tg.arn]
-
-  desired_capacity          = var.asg_desired_capacity
-  min_size                  = var.asg_min_size
-  max_size                  = var.asg_max_size
-  health_check_type         = "ELB"
-  health_check_grace_period = 300
+  # Target Group created by Network Skeleton (fetched via data source)
+  target_group_arns = [data.aws_lb_target_group.notification_tg.arn]
 
   launch_template {
     id      = aws_launch_template.notification_lt.id
     version = "$Latest"
   }
 
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
   tag {
     key                 = "Name"
-    value               = "${var.environment}-${var.application}-asg-instance"
+    value               = "${var.environment}-${var.application}-notification"
     propagate_at_launch = true
   }
 
