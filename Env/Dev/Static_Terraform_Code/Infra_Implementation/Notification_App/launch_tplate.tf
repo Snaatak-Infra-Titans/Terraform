dcaaae7fddd21a5392d23c363895ff77bf4d1788
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-22 14:01:42 +0530

    Update launch_tplate.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/launch_tplate.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/launch_tplate.tf
@@ -1,15 +1,27 @@
 resource "aws_launch_template" "notification_lt" {
-  name_prefix   = "${var.environment}-${var.application}-notification-lt-"
+  name          = "${var.environment}-${var.application}-notification-lt"
   image_id      = data.aws_ami.notification_app.id
   instance_type = var.instance_type
   key_name      = data.aws_key_pair.existing_key.key_name
 
-  
   vpc_security_group_ids = [aws_security_group.notification_sg.id]
 
   iam_instance_profile {
-      name = var.ssm_instance_profile
-    }
+    name = var.ssm_instance_profile
+  }
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    cd /tmp
+    wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+    dpkg -i amazon-ssm-agent.deb
+    systemctl enable amazon-ssm-agent
+    systemctl start amazon-ssm-agent
+    rm amazon-ssm-agent.deb
+    ip link set dev eth0 mtu 1400 || true
+    systemctl restart notification || systemctl start notification
+  EOF
+  )
 
   tag_specifications {
     resource_type = "instance"
@@ -18,7 +30,6 @@ resource "aws_launch_template" "notification_lt" {
     }
   }
 
-  
   lifecycle {
     create_before_destroy = true
   }
