1ada1ae968978326189b83c2d885ff88e21a0a3c
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-23 12:25:44 +0530

    Add outputs for notification security group
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
@@ -0,0 +1,9 @@
+output "notification_sg_id" {
+  description = "The ID of the notification security group"
+  value       = aws_security_group.notification_sg.id
+}
+
+output "notification_sg_name" {
+  description = "The name of the notification security group"
+  value       = aws_security_group.notification_sg.name
+}
