b82ddbbe86072925b99ca97f21a450f7f028f8a4
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-23 23:13:04 +0530

    Create global_variables.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/global_variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" { type = string }
+variable "environment" { type = string }
+variable "application" { type = string }
+variable "cost_center" { type = string }
+variable "owner" { type = string }
+variable "vpc_name" { type = string }
