0dee562e9ae2b47c2a52205e4d4ceb477075fbcd
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-23 22:50:24 +0530

    Update backend.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
-    key          = "network/vpc.tfstate"       # <-- CHANGE THIS LINE
+    key          = "network/vpc.tfstate"       
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
