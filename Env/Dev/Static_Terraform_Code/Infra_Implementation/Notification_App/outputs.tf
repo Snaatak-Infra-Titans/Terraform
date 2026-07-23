697f98153e23a356fa805a1c77069df0048d0673
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-23 17:53:59 +0530

    Update outputs.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
@@ -3,7 +3,7 @@ output "notification_tg_arn" {
   value       = data.aws_lb_target_group.notification_tg.arn
 }
 
-output "notification_tg_id" {
-  description = "The ID of the notification target group fetched via data source"
-  value       = data.aws_lb_target_group.notification_tg.id
+output "notification_tg_name" {
+  description = "The Name of the notification target group fetched via data source"
+  value       = data.aws_lb_target_group.notification_tg.name
 }
