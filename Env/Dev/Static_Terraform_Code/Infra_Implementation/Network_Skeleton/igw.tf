c2a8982d8fee30df3079d7a4911da46b6eadbfd5
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-23 23:17:49 +0530

    Update igw.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/igw.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/igw.tf
@@ -1,12 +1,16 @@
+
+data "aws_vpc" "existing" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+
 resource "aws_internet_gateway" "main_igw" {
-  # IMPLICIT DEPENDENCY: Terraform knows it MUST build the VPC first because we reference its ID here.
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = data.aws_vpc.existing.id
 
   tags = {
-    Name        = var.igw_name
-    Environment = var.environment
-    Owner       = var.owner
-    CostCenter  = var.cost_center
-    Application = var.application
+    Name = var.igw_name
   }
 }
