f60ab4fbf5b3cede372ebe059786df263a243aef
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 12:43:14 +0530

    Update igw.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/igw.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/igw.tf
@@ -1,14 +1,6 @@
-
-data "aws_vpc" "existing" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name]
-  }
-}
-
-
 resource "aws_internet_gateway" "main_igw" {
-  vpc_id = data.aws_vpc.existing.id
+ 
+  vpc_id = aws_vpc.main_vpc.id
 
   tags = {
     Name = var.igw_name
