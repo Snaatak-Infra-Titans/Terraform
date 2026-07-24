fc9df41a56fe97d80051498f7261130cde540309
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 13:17:29 +0530

    Create subnet.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/subnet.tf
@@ -0,0 +1,68 @@
+locals {
+  subnet_plan = {
+    public   = { offset = 0, public = true }
+    frontend = { offset = 2, public = false }
+    backend  = { offset = 4, public = false }
+    database = { offset = 6, public = false }
+  }
+}
+
+resource "aws_subnet" "public" {
+  for_each = { for idx, az in var.azs : az => { az_index = idx } }
+
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, var.subnet_newbits, local.subnet_plan.public.offset + each.value.az_index)
+  availability_zone       = "${var.aws_region}${each.key}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}_${var.application}_public_subnet_${each.key}"
+    Tier = "public"
+    AZ   = "${var.aws_region}${each.key}"
+  }
+}
+
+resource "aws_subnet" "frontend" {
+  for_each = { for idx, az in var.azs : az => { az_index = idx } }
+
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, var.subnet_newbits, local.subnet_plan.frontend.offset + each.value.az_index)
+  availability_zone       = "${var.aws_region}${each.key}"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}_${var.application}_frontend_subnet_${each.key}"
+    Tier = "frontend"
+    AZ   = "${var.aws_region}${each.key}"
+  }
+}
+
+resource "aws_subnet" "backend" {
+  for_each = { for idx, az in var.azs : az => { az_index = idx } }
+
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, var.subnet_newbits, local.subnet_plan.backend.offset + each.value.az_index)
+  availability_zone       = "${var.aws_region}${each.key}"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}_${var.application}_backend_subnet_${each.key}"
+    Tier = "backend"
+    AZ   = "${var.aws_region}${each.key}"
+  }
+}
+
+resource "aws_subnet" "database" {
+  for_each = { for idx, az in var.azs : az => { az_index = idx } }
+
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, var.subnet_newbits, local.subnet_plan.database.offset + each.value.az_index)
+  availability_zone       = "${var.aws_region}${each.key}"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}_${var.application}_database_subnet_${each.key}"
+    Tier = "database"
+    AZ   = "${var.aws_region}${each.key}"
+  }
+}
