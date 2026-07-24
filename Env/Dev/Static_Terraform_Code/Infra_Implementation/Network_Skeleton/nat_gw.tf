9b5cac09829245163fbe9f0276c1fd12e8a4766b
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 13:14:14 +0530

    Update nat_gw.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nat_gw.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nat_gw.tf
@@ -1,6 +1,4 @@
-# -----------------------------------------------------------
-# Elastic IP for NAT Gateway
-# -----------------------------------------------------------
+
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
 
@@ -13,13 +11,11 @@ resource "aws_eip" "nat_eip" {
   }
 }
 
-# -----------------------------------------------------------
-# NAT Gateway
-# -----------------------------------------------------------
+
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   
-  # Link directly to the FIRST public subnet Pawan created (index "a")
+  # Links directly to Pawan's public subnet
   subnet_id     = aws_subnet.public["a"].id
 
   tags = {
@@ -30,15 +26,13 @@ resource "aws_nat_gateway" "nat" {
     CostCenter  = var.cost_center
   }
 
-  # EXPLICIT DEPENDENCY: NAT Gateway requires an IGW to exist in the VPC first!
+  # Explicit dependency on Deepak's IGW
   depends_on = [aws_internet_gateway.main_igw]
 }
 
-# -----------------------------------------------------------
-# Route: Point Private Traffic to the NAT Gateway
-# -----------------------------------------------------------
+
 resource "aws_route" "private_nat_route" {
-  # Link directly to Bhawna's private route table
+  # Links directly to Bhawna's private route table
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
