e3242183fcd1211f4a216992c368edb2db2c2d74
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-24 11:36:37 +0530

    Remove output variables for target group
    Removed output variables for target group ARN and name.
    
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/target_gp.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/target_gp.tf
@@ -35,12 +35,3 @@ resource "aws_lb_target_group" "attendance" {
   }
 }
 
-output "target_group_arn" {
-  description = "The ARN of the Target Group"
-  value       = aws_lb_target_group.attendance.arn
-}
-
-output "target_group_name" {
-  description = "The Name of the Target Group"
-  value       = aws_lb_target_group.attendance.name
-}
