8ccfbc2c0ff653ab457aa1c4e69befdf941aa91f
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 13:53:40 +0530

    Delete Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53_variables.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route53_variables.tf
+++
@@ -1,9 +0,0 @@
-variable "hosted_zone_name" {
-  description = "Name of the public Route 53 hosted zone"
-  type        = string
-}
-
-variable "domain_name" {
-  description = "DNS record that will point to the ALB"
-  type        = string
-}
