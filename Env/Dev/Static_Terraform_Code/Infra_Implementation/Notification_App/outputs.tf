6c2dca85e43c637595ae83828a0811d9f6a439b5
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-24 16:36:13 +0530

    Update outputs.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
@@ -7,3 +7,8 @@ output "notification_sg_name" {
   description = "The name of the notification security group"
   value       = aws_security_group.notification_sg.name
 }
+
+output "packer_builder_security_group_id" {
+  description = "Security Group ID for Packer Builder"
+  value       = aws_security_group.packer_builder_sg.id
+}
