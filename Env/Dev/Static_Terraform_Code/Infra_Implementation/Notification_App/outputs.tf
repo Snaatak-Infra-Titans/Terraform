5eea647a86ac95ae26823035133f7a8f6af25054
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-24 20:22:15 +0530

    Update outputs.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
@@ -1,9 +1,19 @@
-output "notification_tg_arn" {
-  description = "The ARN of the notification target group fetched via data source"
-  value       = data.aws_lb_target_group.notification_tg.arn
+output "notification_lt_id" {
+  description = "The ID of the notification launch template"
+  value       = aws_launch_template.notification_lt.id
 }
 
-output "notification_tg_name" {
-  description = "The Name of the notification target group fetched via data source"
-  value       = data.aws_lb_target_group.notification_tg.name
+output "notification_lt_arn" {
+  description = "The ARN of the notification launch template"
+  value       = aws_launch_template.notification_lt.arn
+}
+
+output "notification_lt_latest_version" {
+  description = "The latest version of the notification launch template"
+  value       = aws_launch_template.notification_lt.latest_version
+}
+
+output "packer_builder_security_group_id" {
+  description = "Security Group ID for Packer Builder"
+  value       = aws_security_group.packer_builder_sg.id
 }
