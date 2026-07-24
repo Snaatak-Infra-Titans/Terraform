b3075714a692a4f0e24b0ee74b418d82308abcc0
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 13:18:00 +0530

    Create route_table.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/route_table.tf
@@ -0,0 +1,49 @@
+# --- PUBLIC ROUTE TABLE ---
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.environment}_${var.application}_public_rt"
+    Tier = "public"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# --- PRIVATE ROUTE TABLE ---
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.environment}_${var.application}_private_rt"
+    Tier = "private"
+  }
+}
+
+resource "aws_route_table_association" "frontend" {
+  for_each       = aws_subnet.frontend
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "backend" {
+  for_each       = aws_subnet.backend
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  for_each       = aws_subnet.database
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
