1824ed4ff1a3eaaf2914d02a04453aab0be59388
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 18:20:47 +0530

    Update global_variables.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/global_variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/global_variables.tf
@@ -3,3 +3,4 @@ variable "environment" { type = string }
 variable "application" { type = string }
 variable "cost_center" { type = string }
 variable "owner"       { type = string }
+variable "vpc_name" { type = string }
