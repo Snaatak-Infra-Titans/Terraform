a6d428bbceb18f661c794d614e9fad400deb9d96
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 13:18:38 +0530

    Update nat_gw.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nat_gw.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/nat_gw.tf
@@ -1,38 +1,27 @@
-
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
 
   tags = {
-    Name        = "${var.environment}_${var.application}_nat_eip"
-    Environment = var.environment
-    Application = var.application
-    Owner       = var.owner
-    CostCenter  = var.cost_center
+    Name = "${var.environment}_${var.application}_nat_eip"
   }
 }
-
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   
-  # Links directly to Pawan's public subnet
+  # Connects directly to Pawan's first public subnet
   subnet_id     = aws_subnet.public["a"].id
 
   tags = {
-    Name        = "${var.environment}_${var.application}_nat_gw"
-    Environment = var.environment
-    Application = var.application
-    Owner       = var.owner
-    CostCenter  = var.cost_center
+    Name = "${var.environment}_${var.application}_nat_gw"
   }
 
-  # Explicit dependency on Deepak's IGW
+  
   depends_on = [aws_internet_gateway.main_igw]
 }
 
 
 resource "aws_route" "private_nat_route" {
-  # Links directly to Bhawna's private route table
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
