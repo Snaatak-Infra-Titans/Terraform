94a6b3e4d392a83160778f2ed2bae37ec74e3830
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-24 16:45:38 +0530

    Add security group for Packer Builder instance
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/sg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/sg.tf
@@ -33,3 +33,32 @@ resource "aws_security_group" "notification_sg" {
     Name = "${var.environment}-${var.application}-notification-sg"
   }
 }
+
+resource "aws_security_group" "packer_builder_sg" {
+  name        = "${var.environment}-${var.application}-packer-builder-sg"
+  description = "Security Group for Packer Builder Instance"
+  vpc_id      = data.aws_vpc.network_vpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow All Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-${var.application}-packer-builder-sg"
+    Environment = var.environment
+    Application = var.application
+    Purpose     = "packer-builder"
+  }
+}
