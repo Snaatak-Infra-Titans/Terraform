de2aa2612fde863f9b098569cb41690670737958
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-24 23:58:34 +0530

    Update instance.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/instance.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/instance.tf
@@ -1,1 +1,14 @@
 
+resource "aws_instance" "frontend" {
+  ami           = var.ami_id
+  instance_type = "t3.micro"
+  
+  # Deploy into the first private frontend subnet
+  subnet_id            = data.terraform_remote_state.network.outputs.frontend_subnet_ids[0]
+  iam_instance_profile = data.aws_iam_instance_profile.ssm_profile.name
+
+  # Only specify resource-specific tags here; default_tags handles the rest
+  tags = {
+    Name = "dev-otms-frontend-instance"
+  }
+}
