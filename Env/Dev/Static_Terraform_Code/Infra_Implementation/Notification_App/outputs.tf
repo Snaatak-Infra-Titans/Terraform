c17652ea08272d23c9551bf9c7a2b3ba1d23b730
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-24 16:47:22 +0530

    Update outputs.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
@@ -2,3 +2,8 @@ output "notification_listener_rule_arn" {
   description = "The ARN of the fetched ALB listener rule for notification routing"
   value       = data.aws_lb_listener_rule.notification_routing.arn
 }
+
+output "packer_builder_security_group_id" {
+  description = "Security Group ID for Packer Builder"
+  value       = aws_security_group.packer_builder_sg.id
+}
