28c70726c30e57f053c02cd21f7376ea37971ee3
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-24 16:46:04 +0530

    Update outputs.tf
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/outputs.tf
@@ -12,3 +12,7 @@ output "notification_asg_arn" {
   description = "The ARN of the notification Auto Scaling Group"
   value       = aws_autoscaling_group.notification_asg.arn
 }
+
+output "packer_builder_security_group_id" {
+    value = aws_security_group.packer_builder_sg.id
+}
