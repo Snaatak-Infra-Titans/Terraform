5c19317c58fe8f2147d419fcad73366f551e6c08
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-26 01:35:18 +0530

    Create data.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/Employee_App/data.tf
@@ -0,0 +1,23 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "otms-terraform-state-dev"
+    key    = "network/full.tfstate"
+    region = var.aws_region
+  }
+}
+
+data "aws_vpc" "main" {
+  id = data.terraform_remote_state.network.outputs.vpc_id
+}
+
+data "aws_route53_zone" "private" {
+  name         = "otms.internal"
+  private_zone = true
+}
+
+# Fetching the Default SG to attach for now, as requested
+data "aws_security_group" "default" {
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+  name   = "default"
+}
