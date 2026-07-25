72c73f3c6041d1e0ae923d3f3643cbf6ef56e684
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-25 16:00:44 +0530

    Update frontend instance configuration
    Added security group association for frontend instance.
    
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/instance.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/instance.tf
@@ -1,13 +1,13 @@
-
 resource "aws_instance" "frontend" {
   ami           = var.ami_id
   instance_type = "t3.micro"
-  
-  # Deploy into the first private frontend subnet
-  subnet_id            = data.terraform_remote_state.network.outputs.frontend_subnet_ids[0]
-  iam_instance_profile = data.aws_iam_instance_profile.ssm_profile.name
 
- 
+  # Deploy into the first private frontend subnet
+  subnet_id              = data.terraform_remote_state.network.outputs.frontend_subnet_ids[0]
+  iam_instance_profile   = data.aws_iam_instance_profile.ssm_profile.name
+  
+  vpc_security_group_ids = [aws_security_group.frontend_sg.id]
+
   tags = {
     Name = "dev-otms-frontend"
   }
