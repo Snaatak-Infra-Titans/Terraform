c97b1bcf5322fef5a2198277dc4df8daa68b1722
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-26 01:50:58 +0530

    Update output.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/Employee_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/Employee_App/output.tf
@@ -1,9 +1,9 @@
+output "employee_api_instance_id" {
+  description = "The ID of the Employee API EC2 instance"
+  value       = aws_instance.employee_api.id
+}
+
 output "employee_api_private_ip" {
   description = "The private IP address of the Employee API instance"
   value       = aws_instance.employee_api.private_ip
-}
-
-output "employee_api_internal_dns" {
-  description = "The Route 53 internal DNS record for the API"
-  value       = aws_route53_record.api_dns.fqdn
 }
