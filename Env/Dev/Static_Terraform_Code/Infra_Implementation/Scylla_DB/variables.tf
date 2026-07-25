7702e41c6de22193fc8cdc4a6a6aae54d8ce73d2
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-25 06:48:33 +0530

    Create variables.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "cost_center" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "ami_owner_id" {
+  type = string
+}
+
+variable "ami_name" {
+  type = string
+}
