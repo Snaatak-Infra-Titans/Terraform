814358fbee057e7168bc53d613b7de2f30a61cf9
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-25 15:59:30 +0530

    Create security group for frontend application
    This security group allows HTTP, HTTPS, custom app port, and SSH traffic while permitting all outbound traffic.
    
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/sg.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "frontend_sg" {
+  name        = "${var.environment}-${var.application}-frontend-sg"
+  description = "Security group for frontend application"
+  
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id 
+
+  ingress {
+    description = "HTTP Traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Custom App Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-${var.application}-frontend-sg"
+  }
+}
