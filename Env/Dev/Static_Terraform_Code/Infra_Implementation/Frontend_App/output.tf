0eb3d906ab55133953da91ceba3a2bf96d9f21c8
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-25 13:19:11 +0530

    Update output.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/output.tf
@@ -1,3 +1,8 @@
+output "frontend_asg_name" {
+  description = "The Name of the Frontend Auto Scaling Group"
+  value       = aws_autoscaling_group.frontend_asg.name
+}
+
 output "frontend_launch_template_id" {
   description = "The ID of the Frontend Launch Template"
   value       = aws_launch_template.frontend.id
@@ -11,14 +16,4 @@ output "frontend_launch_template_latest_version" {
 output "frontend_security_group_id" {
   description = "The ID of the Frontend Security Group"
   value       = aws_security_group.frontend_sg.id
-}
-
-output "frontend_asg_id" {
-  description = "The ID of the Frontend Auto Scaling Group"
-  value       = aws_autoscaling_group.frontend_asg.id
-}
-
-output "frontend_asg_name" {
-  description = "The Name of the Frontend Auto Scaling Group"
-  value       = aws_autoscaling_group.frontend_asg.name
 }
