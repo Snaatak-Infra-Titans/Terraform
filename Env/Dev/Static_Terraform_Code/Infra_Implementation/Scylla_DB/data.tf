3d8c7516d91a46b0aedfbe36bdcc3890f9db26ee
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-25 06:49:26 +0530

    Create data.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/data.tf
@@ -0,0 +1,40 @@
+data "aws_vpc" "network_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnets" "backend_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.network_vpc.id]
+  }
+
+  tags = {
+    Tier = "backend"
+  }
+}
+
+data "aws_key_pair" "existing_key" {
+  key_name = var.key_name
+}
+
+data "aws_security_group" "scylla_sg" {
+  name = "${var.environment}-scylla-sg"
+}
+
+data "aws_iam_instance_profile" "ssm" {
+  name = "dev-otms-ssm-role"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = [var.ami_owner_id]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+}
