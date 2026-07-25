dd5d23d939a42f38486833bd7d4668bad2c1e7bb
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-25 13:47:29 +0530

    Create s3.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/s3.tf
@@ -0,0 +1,53 @@
+resource "aws_s3_bucket" "ansible_ssm" {
+  bucket = "${var.environment}-${var.application}-ansible-ssm"
+
+  tags = {
+    Name         = "${var.environment}-${var.application}-ansible-ssm"
+    Environment  = var.environment
+    Application  = var.application
+    Owner        = var.owner
+    Cost_Center  = var.cost_center
+  }
+}
+
+resource "aws_s3_bucket_versioning" "ansible_ssm" {
+  bucket = aws_s3_bucket.ansible_ssm.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ansible_ssm" {
+  bucket = aws_s3_bucket.ansible_ssm.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "ansible_ssm" {
+  bucket = aws_s3_bucket.ansible_ssm.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "ansible_ssm" {
+  bucket = aws_s3_bucket.ansible_ssm.id
+
+  rule {
+    id     = "cleanup-temp-files"
+    status = "Enabled"
+
+    filter {}
+
+    expiration {
+      days = 1
+    }
+  }
+}
