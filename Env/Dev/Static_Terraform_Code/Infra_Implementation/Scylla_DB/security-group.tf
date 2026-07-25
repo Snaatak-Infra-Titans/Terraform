f293943be3ead7135dfe897c40934e68448cb072
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-25 08:39:10 +0530

    Update security-group.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/security-group.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/security-group.tf
@@ -1,14 +1,44 @@
 resource "aws_security_group" "scylla_sg" {
 
-  name        = "dev-otms-scylla-sg"
+  name        = "${var.environment}-${var.application}-scylla-sg"
   description = "Security Group for ScyllaDB EC2"
-  vpc_id      = data.aws_vpc.network_vpc.id
+
+  vpc_id = data.aws_vpc.network_vpc.id
 
   tags = {
-    Name        = "dev-otms-scylla-sg"
+    Name        = "${var.environment}-${var.application}-scylla-sg"
     Application = var.application
     Environment = var.environment
     Owner       = var.owner
     CostCenter  = var.cost_center
   }
+}
+
+# Ingress Rule
+
+resource "aws_vpc_security_group_ingress_rule" "scylla_cql" {
+
+  security_group_id = aws_security_group.scylla_sg.id
+
+  description = "Allow ScyllaDB CQL traffic from OTMS applications"
+
+  from_port   = 9042
+  to_port     = 9042
+
+  ip_protocol = "tcp"
+
+  cidr_ipv4 = data.aws_vpc.network_vpc.cidr_block
+}
+
+# Egress Rule
+
+resource "aws_vpc_security_group_egress_rule" "all_outbound" {
+
+  security_group_id = aws_security_group.scylla_sg.id
+
+  description = "Allow all outbound traffic"
+
+  ip_protocol = "-1"
+
+  cidr_ipv4 = "0.0.0.0/0"
 }
