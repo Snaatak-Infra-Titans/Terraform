66eeccab7993394e1babaa3c28b1ba3ad59879f5
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-25 08:31:20 +0530

    Add variables for Scylla DB Terraform configuration
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "cost_center" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
