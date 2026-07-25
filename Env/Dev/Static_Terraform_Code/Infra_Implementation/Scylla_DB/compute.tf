696ca7edd776b34a3bc68cdbce6b818d2ad94d20
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-25 06:49:51 +0530

    Create compute.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/compute.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "scylladb" {
+
+  ami                    = data.aws_ami.ubuntu.id
+
+  instance_type          = var.instance_type
+
+  subnet_id              = data.aws_subnets.backend_subnets.ids[0]
+
+  vpc_security_group_ids = [
+    data.aws_security_group.scylla_sg.id
+  ]
+
+  key_name = data.aws_key_pair.existing_key.key_name
+
+  iam_instance_profile = data.aws_iam_instance_profile.ssm.name
+
+  associate_public_ip_address = false
+
+  root_block_device {
+
+    volume_size = 15
+
+    volume_type = "gp3"
+
+    encrypted = true
+
+    delete_on_termination = true
+  }
+
+  tags = {
+
+    Name = "${var.environment}-scylla-ec2"
+
+    Application = "ScyllaDB"
+
+    Environment = var.environment
+
+    Owner = var.owner
+
+    CostCenter = var.cost_center
+  }
+}
