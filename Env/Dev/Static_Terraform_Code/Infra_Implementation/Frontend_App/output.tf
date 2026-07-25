d7c2dfd1abde4a6d06be28e0782f1fce114e1bb8
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-25 16:04:25 +0530

    Add output for frontend Target Group ARN
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/output.tf
@@ -12,3 +12,8 @@ output "frontend_security_group_id" {
   description = "The ID of the Frontend Security Group"
   value       = aws_security_group.frontend_sg.id
 }
+
+output "frontend_tg_arn" {
+  description = "The ARN of the frontend Target Group"
+  value       = aws_lb_target_group.frontend_tg.arn
+}
