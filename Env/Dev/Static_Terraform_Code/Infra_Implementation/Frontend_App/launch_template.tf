60b1fc0a8e0e063ec5f8f952774bc16737a3cc8b
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-25 12:11:31 +0530

    Update launch_template.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/launch_template.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/launch_template.tf
@@ -4,12 +4,12 @@ resource "aws_launch_template" "frontend" {
   instance_type         = "t3.micro"
   vpc_security_group_ids = [aws_security_group.frontend_sg.id]
 
-  # Enables Systems Manager (SSM) without needing an SSH key
+ 
   iam_instance_profile {
     name = data.aws_iam_instance_profile.ssm_profile.name
   }
 
-  # Automatically tags any instance launched by this template
+  
   tag_specifications {
     resource_type = "instance"
     tags = {
