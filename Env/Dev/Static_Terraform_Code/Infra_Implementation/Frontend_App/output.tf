8cd9661e558c72b5f0f4ce20d8e8bf8a3b968790
Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>
2026-07-25 16:01:34 +0530

    Add output for frontend Security Group ID
    Signed-off-by: Shivam-250298 <shivam.uniyal.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/output.tf
@@ -7,3 +7,8 @@ output "frontend_private_ip" {
   description = "The private IP address of the frontend instance"
   value       = aws_instance.frontend.private_ip
 }
+
+output "frontend_sg_id" {
+  description = "The ID of the frontend Security Group"
+  value       = aws_security_group.frontend_sg.id
+}
