8c0cbe1d651a5b157990fafd72cb0bce296b719e
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-25 12:03:34 +0530

    Update backend.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Frontend_App/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "otms-terraform-state-dev"
-    key    = "frontend/instance.tfstate"
+    key    = "frontend/lt.tfstate"
     region = "us-east-1"
   }
 }
