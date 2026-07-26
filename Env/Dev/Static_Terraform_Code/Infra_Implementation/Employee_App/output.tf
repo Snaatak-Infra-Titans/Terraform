a30d6f5f10f365fdcfea8d9a4f0c3952286bc91b
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-26 12:33:14 +0530

    Update output.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/output.tf
@@ -1,12 +1,3 @@
-output "employee_api_instance_id" {
-  description = "The ID of the Employee API EC2 instance"
-  value       = aws_instance.employee_api.id
-}
-
-output "employee_api_private_ip" {
-  description = "The private IP address of the Employee API instance"
-  value       = aws_instance.employee_api.private_ip
-}
 
 output "employee_api_security_group_id" {
   description = "The ID of the custom Security Group attached to the Employee API"
