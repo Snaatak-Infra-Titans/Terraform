d0742cfa49856a3b3b6967b07611531413b0265b
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-26 13:06:50 +0530

    Update compute.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/compute.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/compute.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "redis" {
   associate_public_ip_address  = false
 
   root_block_device {
-    volume_size           = 15
+    volume_size           = 8
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
