12c4c3b9644bbf0fd041af7510182ebc76ad28aa
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-26 13:54:26 +0530

    Update backend.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
-    key          = "redis/instance.tfstate"
+    key          = "redis/instancetest.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
