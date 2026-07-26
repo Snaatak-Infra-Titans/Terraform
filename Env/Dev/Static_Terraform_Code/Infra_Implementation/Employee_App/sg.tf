f403633475d8f213e646cfc333b41cdb2aa5d296
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-26 12:00:38 +0530

    Create sg.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "api_sg" {
+  name        = "dev-otms-employee-api-sg"
+  description = "Security Group for the Employee API backend service"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
+
+  
+  ingress {
+    description     = "Allow HTTP traffic from the ALB on port 8080"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [data.terraform_remote_state.network.outputs.alb_security_group_id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic to databases and internet"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "dev-otms-employee-api-sg"
+    }
+  )
+}
