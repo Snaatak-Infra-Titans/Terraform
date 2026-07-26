de2114c5d7d318b7912215b7006306c7560e759b
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-26 13:37:37 +0530

    Update output.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/output.tf
@@ -1,27 +1,24 @@
-
 output "employee_api_security_group_id" {
-  description = "The ID of the custom Security Group attached to the Employee API"
+  description = "Security Group ID for Employee API"
   value       = aws_security_group.api_sg.id
 }
 
-
 output "employee_api_launch_template_id" {
-  description = "The ID of the Employee API Launch Template"
+  description = "Launch Template ID for Employee API"
   value       = aws_launch_template.employee_api.id
 }
 
-
-output "employee_tg_arn" {
-  description = "The ARN of the Employee Target Group fetched from network state"
-  value       = data.terraform_remote_state.network.outputs.target_group_arns["employee"]
-}
-
 output "employee_api_asg_name" {
-  description = "The name of the Employee API Auto Scaling Group"
+  description = "Auto Scaling Group Name"
   value       = aws_autoscaling_group.employee_api.name
 }
 
-output "employee_api_cpu_policy_arn" {
-  description = "The ARN of the CPU auto-scaling policy"
-  value       = aws_autoscaling_policy.cpu_scaling.arn
+output "employee_tg_arn" {
+  description = "Employee Target Group ARN fetched from network state"
+  value       = data.terraform_remote_state.network.outputs.target_group_arns["employee"]
+}
+
+output "employee_listener_rule_arn" {
+  description = "Employee ALB Listener Rule ARN fetched from network state"
+  value       = data.terraform_remote_state.network.outputs.listener_rule_arns["employee"]
 }
