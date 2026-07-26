5d2e99301268fc48c09bd69c0636655107c6c3e2
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-26 13:33:18 +0530

    Update outputs.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/outputs.tf
@@ -1,7 +1,11 @@
 output "security_group_id" {
+  description = "Redis Security Group ID"
+
   value = aws_security_group.redis_sg.id
 }
 
 output "security_group_name" {
+  description = "Redis Security Group Name"
+
   value = aws_security_group.redis_sg.name
 }
