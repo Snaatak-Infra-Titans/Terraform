875f59e82c679e5ed6c0de8b3a99ed3709f73fe5
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-26 13:05:48 +0530

    Create data.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/data.tf
@@ -0,0 +1,48 @@
+data "aws_vpc" "network_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnet" "database_subnet" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.network_vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [var.database_subnet_name]
+  }
+}
+
+data "aws_subnet" "backend_subnet" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.network_vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [var.backend_subnet_name]
+  }
+}
+
+data "aws_key_pair" "existing_key" {
+  key_name = var.key_name
+}
+
+data "aws_iam_instance_profile" "ssm" {
+  name = "dev-otms-ssm-role"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = [var.ami_owner_id]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+}
