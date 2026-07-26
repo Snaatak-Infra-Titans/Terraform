18e0a197c49c7cae71ddf2c05e7b7339d86c0e02
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-26 13:09:45 +0530

    Create sg.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/sg.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "redis_sg" {
+  name        = "${var.environment}-${var.application}-redis-sg"
+  description = "Security Group for Redis EC2"
+  vpc_id      = data.aws_vpc.network_vpc.id
+
+  tags = {
+    Name        = "${var.environment}-${var.application}-redis-sg"
+    Application = var.application
+    Environment = var.environment
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+  }
+}
+
+# Ingress Rule
+resource "aws_vpc_security_group_ingress_rule" "redis" {
+  security_group_id = aws_security_group.redis_sg.id
+  description       = "Allow Redis traffic from Backend subnet"
+  from_port         = 6379
+  to_port           = 6379
+  ip_protocol       = "tcp"
+  cidr_ipv4         = data.aws_subnet.backend_subnet.cidr_block
+}
+
+# Egress Rule
+resource "aws_vpc_security_group_egress_rule" "all_outbound" {
+  security_group_id = aws_security_group.redis_sg.id
+  description       = "Allow all outbound traffic"
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
