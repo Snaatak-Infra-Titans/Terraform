2eddfc27ce99dda53af818d6782feebeae6ed76e
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-26 13:06:18 +0530

    Create compute.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/compute.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "redis" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnet.database_subnet.id
+  vpc_security_group_ids       = [aws_security_group.redis_sg.id]
+  iam_instance_profile         = data.aws_iam_instance_profile.ssm.name
+  key_name                    = data.aws_key_pair.existing_key.key_name
+  associate_public_ip_address  = false
+
+  root_block_device {
+    volume_size           = 15
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name        = "${var.environment}-${var.application}-redis-ec2"
+    Application = var.application
+    Environment = var.environment
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+  }
+}
