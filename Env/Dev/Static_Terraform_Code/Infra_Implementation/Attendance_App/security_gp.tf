24e1e1853b869be0ba740a977bdb48408f5822df
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-26 17:46:00 +0530

    Refactor security group for Attendance App
    Updated security group and ingress rules for Attendance App.
    
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/security_gp.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/security_gp.tf
@@ -1,26 +1,19 @@
 data "aws_vpc" "main" {
-  tags = { Name = "${var.environment}-vpc" }
-}
-
-locals {
-  ingress_rules = [
-    { port = 8080, cidr = [data.aws_vpc.main.cidr_block] },
-    { port = 22,   cidr = ["0.0.0.0/0"] }
-  ]
+  tags = { Name = "${var.environment}-otms-vpc" }
 }
 
 # Attendance Service Security Group
 resource "aws_security_group" "attendance" {
-  name   = "${var.environment}-attendance-sg"
+  name   = "${var.environment}-otms-attendance-sg"
   vpc_id = data.aws_vpc.main.id
 
   dynamic "ingress" {
-    for_each = local.ingress_rules
+    for_each = var.ingress_rules
     content {
       from_port   = ingress.value.port
       to_port     = ingress.value.port
       protocol    = "tcp"
-      cidr_blocks = ingress.value.cidr
+      cidr_blocks = [for c in ingress.value.cidr : c == "vpc" ? data.aws_vpc.main.cidr_block : c]
     }
   }
 
@@ -32,15 +25,11 @@ resource "aws_security_group" "attendance" {
   }
 
   tags = {
-    Name        = "${var.environment}-attendance-sg"
-    Application = var.tag_application
-    Owner       = var.tag_owner
+    Name        = "${var.environment}-otms-attendance-sg"
+    Application = var.application
+    Owner       = var.owner
     Environment = var.environment
-    CostCenter  = var.tag_costcenter
+    CostCenter  = var.cost_center
   }
 }
 
-output "attendance_security_group_id" {
-  description = "The ID of the Attendance security group"
-  value       = aws_security_group.attendance.id
-}
