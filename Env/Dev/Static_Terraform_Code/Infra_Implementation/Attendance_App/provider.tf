8af41f13553e9576d3767d317c858db8d1749a35
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-27 22:32:40 +0530

    Update provider.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/provider.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/provider.tf
@@ -1,18 +1,11 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
-
   default_tags {
-    tags = var.common_tags
+    tags = {
+      Application = var.application
+      Environment = var.environment
+      Owner       = var.owner
+      CostCenter  = var.cost_center
+    }
   }
 }
