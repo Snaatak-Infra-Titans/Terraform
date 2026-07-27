acd4dbd64fdd01903d7f2b8cca3e2f8a30ac1f9f
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-27 22:01:37 +0530

    Update data.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/data.tf
@@ -1,67 +1,68 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config = {
-    bucket = "otms-terraform-state-dev-788572613316"
-    key    = "network/vpc.tfstate"
-    region = "us-east-1"
-  }
-}
-
-data "terraform_remote_state" "subnets" {
-  backend = "s3"
-
-  config = {
-    bucket = "otms-terraform-state-dev-788572613316"
-    key    = "network/subnets.tfstate"
-    region = "us-east-1"
-  }
-}
-
-data "terraform_remote_state" "alb" {
-  backend = "s3"
-
-  config = {
-    bucket = "otms-terraform-state-dev-788572613316"
-    key    = "network/alb.tfstate"
-    region = "us-east-1"
-  }
-}
-
-data "terraform_remote_state" "ssh" {
-  backend = "s3"
-
-  config = {
-    bucket = "otms-terraform-state-dev-788572613316"
-    key    = "network/ssh.tfstate"
-    region = "us-east-1"
-  }
-}
-
-data "aws_lb_target_group" "attendance" {
-  name = "dev-otms-attendance-tg"
-}
-
-data "aws_security_group" "alb" {
+data "aws_vpc" "network_vpc" {
   filter {
     name   = "tag:Name"
-    values = ["dev-otms-alb-sg"]
+    values = [var.vpc_name]
   }
-
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
-data "aws_subnets" "backend" {
+data "aws_subnets" "backend_subnets" {
   filter {
     name   = "vpc-id"
-    values = [data.terraform_remote_state.vpc.outputs.vpc_id]
+    values = [data.aws_vpc.network_vpc.id]
+  }
+  tags = {
+    Tier = "backend"
+  }
+}
+
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.network_vpc.id]
   }
 
+  tags = {
+    Tier = "public"
+  }
+}
+
+# Fetching the existing ALB from Network Skeleton
+data "aws_lb" "existing_alb" {
+  name = "${var.environment}-${var.application}-alb"
+}
+
+# Fetching the HTTPS Listener (Port 443) for routing rules
+data "aws_lb_listener" "app_listener" {
+  load_balancer_arn = data.aws_lb.existing_alb.arn
+  port              = 443
+}
+
+data "aws_security_group" "alb_sg" {
   filter {
-    name = "tag:Name"
-    values = [
-      "dev_otms_backend_subnet_a",
-      "dev_otms_backend_subnet_b"
-    ]
+    name   = "tag:Name"
+    values = ["*alb*"]
+  }
+}
+
+/*
+data "aws_security_group" "bastion_sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["*bastion*"]
+  }
+}
+*/
+
+data "aws_key_pair" "existing_key" {
+  key_name = var.key_name
+}
+
+data "aws_ami" "notification_app" {
+  most_recent = true
+  owners      = [var.ami_owner_id]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
   }
 }
