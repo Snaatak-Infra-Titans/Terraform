73b41ff28c28b0237d490baea978d21aa42f529c
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-27 13:43:36 +0530

    Add Terraform variables for infrastructure setup
    Define variables for AWS region, environment, application, owner, and cost center.
    
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/variable.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "application" {
+  type    = string
+  default = "otms"
+}
+
+variable "owner" {
+  type    = string
+  default = "Infra-Titans"
+}
+
+variable "cost_center" {
+  type    = string
+  default = "Snaatak"
+}
+
