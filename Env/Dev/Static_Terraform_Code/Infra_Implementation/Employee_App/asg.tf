7f57cf37fe4a6795c4001d04511f03993f9d8fe8
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-27 12:18:33 +0530

    Update asg.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/asg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/asg.tf
@@ -1,16 +1,13 @@
 resource "aws_autoscaling_group" "employee_api" {
   name = "dev-otms-employee-api-asg"
 
-
   vpc_zone_identifier = [data.terraform_remote_state.network.outputs.backend_subnet_ids[0]]
 
   desired_capacity = 1
   min_size         = 1
   max_size         = 2
 
-
   target_group_arns = [data.terraform_remote_state.network.outputs.target_group_arns["employee"]]
-
 
   health_check_type         = "ELB"
   health_check_grace_period = 300
@@ -20,13 +17,11 @@ resource "aws_autoscaling_group" "employee_api" {
     version = "$Latest"
   }
 
-
   tag {
     key                 = "Name"
     value               = "dev-otms-employee-api-asg-instance"
     propagate_at_launch = true
   }
-
 
   dynamic "tag" {
     for_each = var.common_tags
