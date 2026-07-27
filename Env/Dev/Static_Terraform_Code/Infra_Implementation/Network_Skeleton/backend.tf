ca86f190916a4ff8f41fbac4945a1a5aa5c07ec8
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-27 13:40:55 +0530

    Add Terraform backend configuration for S3
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "otms-terraform-state-dev"
+    key          = "network/route-tables.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
