5eafbf01b2c14b4a1520c935123cbb71734a754a
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-27 22:44:12 +0530

    Update output.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/output.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/output.tf
@@ -1,30 +1,14 @@
-output "attendance_api_security_group_id" {
-  description = "The ID of the Security Group attached to the Attendance API"
-  value       = aws_security_group.attendance_api_sg.id
+output "attendance_lt_id" {
+  description = "The ID of the attendance launch template"
+  value       = aws_launch_template.attendance_lt.id
 }
 
-output "attendance_api_launch_template_id" {
-  description = "The ID of the Attendance API Launch Template"
-  value       = aws_launch_template.attendance_api.id
+output "attendance_lt_arn" {
+  description = "The ARN of the notification launch template"
+  value       = aws_launch_template.attendance_lt.arn
 }
 
-output "attendance_api_launch_template_latest_version" {
-  description = "The latest version of the Attendance API Launch Template"
-  value       = aws_launch_template.attendance_api.latest_version
+output "attendance_lt_latest_version" {
+  description = "The latest version of the attendance launch template"
+  value       = aws_launch_template.attendance_lt.latest_version
 }
-
-output "attendance_tg_arn" {
-  description = "The ARN of the Attendance target group fetched from the network state"
-  value       = data.aws_lb_target_group.attendance.arn
-}
-
-output "attendance_api_asg_name" {
-  description = "The name of the Attendance API Auto Scaling Group"
-  value       = aws_autoscaling_group.attendance_api.name
-}
-
-output "attendance_api_cpu_policy_arn" {
-  description = "The ARN of the Attendance API CPU auto-scaling policy"
-  value       = aws_autoscaling_policy.attendance_cpu_scaling.arn
-}
-
