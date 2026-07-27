a56b68192c0c87af3eb8fad469c4143de7221dc1
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-27 23:00:24 +0530

    Update variables.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/variables.tf
@@ -48,3 +48,23 @@ variable "ssm_instance_profile" {
   type        = string
   description = "Name of the manually created IAM Instance Profile for SSM"
 }
+
+variable "asg_min_size" {
+  type        = number
+  description = "Minimum number of instances in ASG"
+}
+
+variable "asg_max_size" {
+  type        = number
+  description = "Maximum number of instances in ASG"
+}
+
+variable "asg_desired_capacity" {
+  type        = number
+  description = "Desired number of instances in ASG"
+}
+
+variable "cpu_target_value" {
+  type        = number
+  description = "Target CPU utilization percentage for ASG scaling"
+}
