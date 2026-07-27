29fa60ae1377ba8815464824095b74ed7ee9062f
Pawan2031 <pawan.tiwari.snaatak@mygurukulam.co>
2026-07-27 12:05:56 +0530

    Refactor ASG and Launch Template configurations
    Signed-off-by: Pawan2031 <pawan.tiwari.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/asg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/asg.tf
@@ -1,73 +1,34 @@
-# Standard Tags Local Block (Acceptance Criteria 5)
-locals {
-  common_tags = {
-    Application = var.application_name
-    Owner       = var.owner
-    Environment = var.environment
-    CostCenter  = var.cost_center
-  }
-}
+resource "aws_autoscaling_group" "employee_api" {
+  name = "dev-otms-employee-api-asg"
 
-# Launch Template (Configured in Ticket 431)
-resource "aws_launch_template" "employee_app" {
-  name_prefix   = "${var.environment}-${var.application_name}-lt-"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
+  vpc_zone_identifier = [data.terraform_remote_state.network.outputs.backend_subnet_ids[0]]
 
-  vpc_security_group_ids = var.security_group_ids
-  key_name               = var.key_name
+  desired_capacity = 1
+  min_size         = 1
+  max_size         = 2
 
-  iam_instance_profile {
-    name = var.iam_instance_profile_name
-  }
-
-  monitoring {
-    enabled = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = merge(local.common_tags, {
-      Name = "${var.environment}-${var.application_name}-instance"
-    })
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Auto Scaling Group (Ticket 433 - Pawan)
-resource "aws_autoscaling_group" "employee_app_asg" {
-  name_prefix         = "${var.environment}-${var.application_name}-asg-"
-  vpc_zone_identifier = var.vpc_zone_identifier
-
-  min_size         = var.asg_min_size
-  max_size         = var.asg_max_size
-  desired_capacity = var.asg_desired_capacity
-
-  # Attached to Target Group created by Deepak in Ticket 432
-  target_group_arns = var.target_group_arns
-
-  launch_template {
-    id      = aws_launch_template.employee_app.id
-    version = "$Latest"
-  }
+  target_group_arns = [data.terraform_remote_state.network.outputs.target_group_arns["employee"]]
 
   health_check_type         = "ELB"
   health_check_grace_period = 300
 
-  # Propagate standard tags to instances launched by ASG
+  launch_template {
+    id      = aws_launch_template.employee_api.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "dev-otms-employee-api-asg-instance"
+    propagate_at_launch = true
+  }
+
   dynamic "tag" {
-    for_each = local.common_tags
+    for_each = var.common_tags
     content {
       key                 = tag.key
       value               = tag.value
       propagate_at_launch = true
     }
-  }
-
-  lifecycle {
-    create_before_destroy = true
   }
 }
