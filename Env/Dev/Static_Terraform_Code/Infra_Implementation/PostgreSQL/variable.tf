0e961bafec1cfff3bd62bbedc67cfc42abf15ed8
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-27 23:29:07 +0530

    Create variable.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/PostgreSQL/variable.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "cost_center" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "database_subnet_name" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_owner_id" {
+  type        = string
+  description = "AWS Account ID of the AMI owner"
+}
+
+variable "ami_name" {
+  type        = string
+  description = "AMI name"
+}
