33dd1959ea14964dc34be00cf4d7b8cbbc93f5a5
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 18:56:55 +0530

    Create variables.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "cost_center" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
