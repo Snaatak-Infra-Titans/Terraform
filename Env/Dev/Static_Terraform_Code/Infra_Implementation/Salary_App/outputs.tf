79ca8b3393ad447327aa859d2ee878068d8ea155
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 22:45:33 +0530

    Update outputs.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
@@ -1,14 +1,9 @@
-output "salary_lt_id" {
-  description = "The ID of the salary launch template"
-  value       = aws_launch_template.salary_lt.id
+output "cpu_target_tracking_policy_arn" {
+  description = "The ARN of the CPU target tracking auto scaling policy"
+  value       = aws_autoscaling_policy.cpu_target_tracking.arn
 }
 
-output "salary_lt_arn" {
-  description = "The ARN of the salary launch template"
-  value       = aws_launch_template.salary_lt.arn
-}
-
-output "salary_lt_latest_version" {
-  description = "The latest version of the salary launch template"
-  value       = aws_launch_template.salary_lt.latest_version
+output "cpu_target_tracking_policy_name" {
+  description = "The name of the CPU target tracking auto scaling policy"
+  value       = aws_autoscaling_policy.cpu_target_tracking.name
 }
