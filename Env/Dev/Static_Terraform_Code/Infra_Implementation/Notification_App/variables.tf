f64a83ca30e69a322cd3c5418b8070a577af5c79
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 19:42:37 +0530

    Create variables.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/variables.tf
@@ -0,0 +1,46 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "cost_center" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_owner_id" {
+  type        = string
+  description = "AWS Account ID of the Custom AMI owner"
+}
+
+variable "ami_name" {
+  type        = string
+  description = "Name of the golden AMI for the Notification app"
+}
+
+variable "ssm_instance_profile" {
+  type        = string
+  description = "Name of the manually created IAM Instance Profile for SSM"
+}
