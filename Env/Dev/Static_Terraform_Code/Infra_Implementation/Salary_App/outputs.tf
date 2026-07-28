54984c0a406f3e0f3b4f5188f5bec5247234a663
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 22:34:32 +0530

    Update outputs.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
@@ -1,4 +1,4 @@
 output "salary_listener_rule_arn" {
-  description = "The ARN of the fetched ALB listener rule for notification routing"
-  value       = data.aws_lb_listener_rule.notification_routing.arn
+  description = "The ARN of the fetched ALB listener rule for salary routing"
+  value       = data.aws_lb_listener_rule.salary_routing.arn
 }
