364be8f0d674823ca055881e5150d23e018c788e
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 22:32:27 +0530

    Update outputs.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
@@ -1,14 +1,4 @@
-output "salary_lt_id" {
-  description = "The ID of the salary launch template"
-  value       = aws_launch_template.salary_lt.id
-}
-
-output "salary_lt_arn" {
-  description = "The ARN of the salary launch template"
-  value       = aws_launch_template.salary_lt.arn
-}
-
-output "salary_lt_latest_version" {
-  description = "The latest version of the salary launch template"
-  value       = aws_launch_template.salary_lt.latest_version
+output "notification_listener_rule_arn" {
+  description = "The ARN of the fetched ALB listener rule for notification routing"
+  value       = data.aws_lb_listener_rule.notification_routing.arn
 }
