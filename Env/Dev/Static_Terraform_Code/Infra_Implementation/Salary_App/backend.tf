af6acea4ceae127f909a05040a0ffc78f1595928
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 20:00:01 +0530

    Update backend.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
-    key          = "notification/lt.tfstate"
+    key          = "salary/lt.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
