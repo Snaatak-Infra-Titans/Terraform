571de732d055b86991b127027903f42fa6334737
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 19:06:20 +0530

    Update outputs.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
@@ -1,9 +1,9 @@
 output "salary_sg_id" {
   description = "The ID of the notification security group"
-  value       = aws_security_group.notification_sg.id
+  value       = aws_security_group.salary_sg.id
 }
 
 output "salary_sg_name" {
-  description = "The name of the notification security group"
-  value       = aws_security_group.notification_sg.name
+  description = "The name of the salary security group"
+  value       = aws_security_group.salary_sg.name
 }
