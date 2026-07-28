7c3cc77cf69556923623b3791e40eaa255639c19
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 19:14:12 +0530

    Update launch_template.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/launch_template.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/launch_template.tf
@@ -1,73 +1,27 @@
-resource "aws_launch_template" "salary" {
-  name_prefix = "${var.environment}-salary-lt-"
-
-  image_id      = var.salary_ami_id
+resource "aws_launch_template" "salary_lt" {
+  name          = "${var.environment}-${var.application}-salary-lt"
+  image_id      = data.aws_ami.salary_app.id
   instance_type = var.instance_type
+  key_name      = data.aws_key_pair.existing_key.key_name
 
-  update_default_version = true
+  vpc_security_group_ids = [
+    aws_security_group.salary_sg.id
+  ]
 
   iam_instance_profile {
-    name = var.instance_profile_name
-  }
-
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups             = [var.salary_security_group_id]
-    delete_on_termination       = true
-  }
-
-  monitoring {
-    enabled = true
-  }
-
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 2
-    instance_metadata_tags      = "enabled"
-  }
-
-  block_device_mappings {
-    device_name = "/dev/sda1"
-
-    ebs {
-      volume_size           = 20
-      volume_type           = "gp3"
-      delete_on_termination = true
-      encrypted             = true
-    }
+    name = var.ssm_instance_profile
   }
 
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name        = "${var.environment}-salary-instance"
-      Application = var.application
+      Name        = "${var.environment}-${var.application}-salary"
       Environment = var.environment
+      Application = var.application
       Owner       = var.owner
       CostCenter  = var.cost_center
     }
-  }
-
-  tag_specifications {
-    resource_type = "volume"
-
-    tags = {
-      Name        = "${var.environment}-salary-volume"
-      Application = var.application
-      Environment = var.environment
-      Owner       = var.owner
-      CostCenter  = var.cost_center
-    }
-  }
-
-  tags = {
-    Name        = "${var.environment}-salary-launch-template"
-    Application = var.application
-    Environment = var.environment
-    Owner       = var.owner
-    CostCenter  = var.cost_center
   }
 
   lifecycle {
