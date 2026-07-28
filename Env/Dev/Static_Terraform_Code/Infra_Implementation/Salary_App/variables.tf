2fbb79ca27b2516b4bb90654d1c364d077c7d5e6
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-28 22:15:40 +0530

    Update variables.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/variables.tf
@@ -30,6 +30,26 @@ variable "instance_type" {
   type = string
 }
 
+variable "asg_min_size" {
+  type        = number
+  description = "Minimum number of instances in ASG"
+}
+
+variable "asg_max_size" {
+  type        = number
+  description = "Maximum number of instances in ASG"
+}
+
+variable "asg_desired_capacity" {
+  type        = number
+  description = "Desired number of instances in ASG"
+}
+
+variable "cpu_target_value" {
+  type        = number
+  description = "Target CPU utilization percentage for ASG scaling"
+}
+
 variable "ami_owner_id" {
   type        = string
   description = "AWS Account ID of the Custom AMI owner"
