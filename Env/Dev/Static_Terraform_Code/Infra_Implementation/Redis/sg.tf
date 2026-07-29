1cb43f12eb59686a25511a84059e249b28b46af5
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-30 00:58:56 +0530

    Update sg.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/sg.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/sg.tf
@@ -1,3 +1,7 @@
+###############################################################################
+# Redis Security Group
+###############################################################################
+
 resource "aws_security_group" "redis_sg" {
   name        = "${var.environment}-${var.application}-redis-sg"
   description = "Security Group for Redis EC2"
@@ -12,35 +16,44 @@ resource "aws_security_group" "redis_sg" {
   }
 }
 
-# Ingress Rule
-resource "aws_vpc_security_group_ingress_rule" "redis" {
-  security_group_id = aws_security_group.redis_sg.id
-  description       = "Allow Redis traffic from Backend subnet"
-  from_port         = 6379
-  to_port           = 6379
-  ip_protocol       = "tcp"
-  security_group_id = data.aws_security_group.salary_sg.id
-}
-/*
-resource "aws_vpc_security_group_ingress_rule" "redis" {
-  security_group_id = aws_security_group.redis_sg.id
-  description       = "Allow Redis traffic from Backend subnet"
-  from_port         = 6379
-  to_port           = 6379
-  ip_protocol       = "tcp"
-  security_group_id = data.aws_security_group.employee_sg.id
+###############################################################################
+# Ingress Rules
+###############################################################################
+
+resource "aws_vpc_security_group_ingress_rule" "salary_to_redis" {
+  security_group_id            = aws_security_group.redis_sg.id
+  referenced_security_group_id = data.aws_security_group.salary_sg.id
+
+  description = "Allow Redis from Salary API"
+  from_port   = 6379
+  to_port     = 6379
+  ip_protocol = "tcp"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "redis" {
-  security_group_id = aws_security_group.redis_sg.id
-  description       = "Allow Redis traffic from Backend subnet"
-  from_port         = 6379
-  to_port           = 6379
-  ip_protocol       = "tcp"
-  security_group_id = data.aws_security_group.attendance_sg.id
+resource "aws_vpc_security_group_ingress_rule" "employee_to_redis" {
+  security_group_id            = aws_security_group.redis_sg.id
+  referenced_security_group_id = data.aws_security_group.employee_sg.id
+
+  description = "Allow Redis from Employee API"
+  from_port   = 6379
+  to_port     = 6379
+  ip_protocol = "tcp"
 }
-*/
+
+resource "aws_vpc_security_group_ingress_rule" "attendance_to_redis" {
+  security_group_id            = aws_security_group.redis_sg.id
+  referenced_security_group_id = data.aws_security_group.attendance_sg.id
+
+  description = "Allow Redis from Attendance API"
+  from_port   = 6379
+  to_port     = 6379
+  ip_protocol = "tcp"
+}
+
+###############################################################################
 # Egress Rule
+###############################################################################
+
 resource "aws_vpc_security_group_egress_rule" "all_outbound" {
   security_group_id = aws_security_group.redis_sg.id
   description       = "Allow all outbound traffic"
