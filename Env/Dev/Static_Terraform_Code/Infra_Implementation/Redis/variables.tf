aa66048394641ea261d882bdf1087f58dc8c0463
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-07-30 00:53:51 +0530

    Update variables.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Redis/variables.tf
@@ -26,7 +26,27 @@ variable "database_subnet_name" {
   type = string
 }
 
+variable "key_name" {
+  type        = string
+  description = "Existing EC2 Key Pair name"
+}
+
+variable "ami_name" {
+  type        = string
+  description = "Name of the AMI used for Redis EC2"
+}
+
+variable "ami_owner_id" {
+  type        = string
+  description = "AWS Account ID that owns the AMI"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+}
+
 variable "ssm_instance_profile" {
   type        = string
-  description = "Name of the manually created IAM Instance Profile for SSM"
+  description = "Name of the existing IAM Instance Profile for SSM"
 }
