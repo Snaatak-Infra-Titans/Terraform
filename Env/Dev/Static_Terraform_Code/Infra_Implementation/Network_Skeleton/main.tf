a6a8dc6ef79c74e4d14b3a8cd53ca86793e753f1
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-30 08:52:38 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/main.tf
@@ -1,4 +1,3 @@
-# Public Subnet
 data "aws_subnet" "public" {
   filter {
     name   = "tag:Name"
@@ -6,41 +5,31 @@ data "aws_subnet" "public" {
   }
 }
 
-# Internet Gateway
-data "aws_internet_gateway" "igw" {
-  filter {
-    name   = "tag:Name"
-    values = [var.internet_gateway_name]
-  }
-}
-
-# Private Route Table
-data "aws_route_table" "private" {
-  filter {
-    name   = "tag:Name"
-    values = [var.private_route_table_name]
-  }
-}
-
 resource "aws_eip" "this" {
   domain = "vpc"
 
-  tags = {
-    Name = var.nat_eip_name
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.eip_name
+    }
+  )
 }
 
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.this.id
   subnet_id     = data.aws_subnet.public.id
 
-  tags = {
-    Name = var.nat_gateway_name
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = var.nat_gateway_name
+    }
+  )
 }
 
 resource "aws_route" "private_nat" {
-  route_table_id         = data.aws_route_table.private.id
+  route_table_id         = var.private_route_table_id
   destination_cidr_block = var.destination_cidr
   nat_gateway_id         = aws_nat_gateway.this.id
 }
