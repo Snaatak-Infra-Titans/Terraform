d18abaab5d11dfed11cca6bb6cc4768fe8e336ea
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-30 21:58:34 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/main.tf
@@ -6,7 +6,6 @@ data "aws_vpc" "network_vpc" {
 }
 
 data "aws_subnet" "database_subnet" {
-
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.network_vpc.id]
@@ -18,17 +17,7 @@ data "aws_subnet" "database_subnet" {
   }
 }
 
-
-data "aws_key_pair" "existing_key" {
-  key_name = var.key_name
-}
-
-data "aws_iam_instance_profile" "ssm" {
-  name = "dev-otms-ssm-role"
-}
-
 data "aws_ami" "ubuntu" {
-
   most_recent = true
 
   owners = [var.ami_owner_id]
@@ -39,8 +28,53 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_instance" "scylladb" {
+data "aws_key_pair" "existing_key" {
+  key_name = var.key_name
+}
 
+data "aws_iam_instance_profile" "ssm" {
+  name = "dev-otms-ssm-role"
+}
+
+resource "aws_security_group" "scylla_sg" {
+  name        = "${var.environment}-${var.application}-scylla-sg"
+  description = "Security Group for ScyllaDB EC2"
+
+  vpc_id = data.aws_vpc.network_vpc.id
+
+  tags = {
+    Name        = "${var.environment}-${var.application}-scylla-sg"
+    Application = var.application
+    Environment = var.environment
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "scylla_cql" {
+  security_group_id = aws_security_group.scylla_sg.id
+
+  description = "Allow ScyllaDB CQL traffic from OTMS applications"
+
+  from_port = 9042
+  to_port   = 9042
+
+  ip_protocol = "tcp"
+
+  cidr_ipv4 = data.aws_vpc.network_vpc.cidr_block
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_outbound" {
+  security_group_id = aws_security_group.scylla_sg.id
+
+  description = "Allow all outbound traffic"
+
+  ip_protocol = "-1"
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_instance" "scylladb" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
@@ -57,7 +91,6 @@ resource "aws_instance" "scylladb" {
   associate_public_ip_address = false
 
   root_block_device {
-
     volume_size = 15
 
     volume_type = "gp3"
@@ -68,61 +101,10 @@ resource "aws_instance" "scylladb" {
   }
 
   tags = {
-
-    Name = "${var.environment}-${var.application}-scylla-ec2"
-
+    Name        = "${var.environment}-${var.application}-scylla-ec2"
     Application = "otms"
-
-    Environment = var.environment
-
-    Owner = var.owner
-
-    CostCenter = var.cost_center
-  }
-
-}
-
-resource "aws_security_group" "scylla_sg" {
-
-  name        = "${var.environment}-${var.application}-scylla-sg"
-  description = "Security Group for ScyllaDB EC2"
-
-  vpc_id = data.aws_vpc.network_vpc.id
-
-  tags = {
-    Name        = "${var.environment}-${var.application}-scylla-sg"
-    Application = var.application
     Environment = var.environment
     Owner       = var.owner
     CostCenter  = var.cost_center
   }
-}
-
-# Ingress Rule
-
-resource "aws_vpc_security_group_ingress_rule" "scylla_cql" {
-
-  security_group_id = aws_security_group.scylla_sg.id
-
-  description = "Allow ScyllaDB CQL traffic from OTMS applications"
-
-  from_port = 9042
-  to_port   = 9042
-
-  ip_protocol = "tcp"
-
-  cidr_ipv4 = data.aws_vpc.network_vpc.cidr_block
-}
-
-# Egress Rule
-
-resource "aws_vpc_security_group_egress_rule" "all_outbound" {
-
-  security_group_id = aws_security_group.scylla_sg.id
-
-  description = "Allow all outbound traffic"
-
-  ip_protocol = "-1"
-
-  cidr_ipv4 = "0.0.0.0/0"
 }
