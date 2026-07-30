fcaad2ba15a3d7e25532b15b7a3dc5d70ab19f4e
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-30 22:02:57 +0530

    Update variables.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/variables.tf
@@ -1,45 +1,59 @@
 variable "aws_region" {
-  type = string
+  description = "AWS Region"
+  type        = string
 }
 
 variable "environment" {
-  type = string
+  description = "Deployment environment"
+  type        = string
 }
 
 variable "application" {
-  type = string
+  description = "Application name"
+  type        = string
 }
 
 variable "owner" {
-  type = string
+  description = "Resource owner"
+  type        = string
 }
 
 variable "cost_center" {
-  type = string
+  description = "Cost center"
+  type        = string
 }
 
 variable "vpc_name" {
-  type = string
+  description = "Name tag of the existing VPC"
+  type        = string
 }
 
 variable "database_subnet_name" {
-  type = string
+  description = "Name tag of the existing database subnet"
+  type        = string
 }
 
 variable "key_name" {
-  type = string
+  description = "Existing EC2 Key Pair name"
+  type        = string
 }
 
 variable "instance_type" {
-  type = string
+  description = "EC2 instance type"
+  type        = string
 }
 
 variable "ami_owner_id" {
-  type        = string
   description = "AWS Account ID of the AMI owner"
+  type        = string
 }
 
 variable "ami_name" {
-  type        = string
   description = "AMI name"
+  type        = string
+}
+
+variable "ssm_instance_profile" {
+  description = "IAM Instance Profile attached to the EC2 instance"
+  type        = string
 }
