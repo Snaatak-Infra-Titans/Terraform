edfdf86cb63c4fba14627710a89fe5287af9913d
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-30 08:47:01 +0530

    Update provider.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/provider.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/provider.tf
@@ -1,21 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-  default_tags {
-    tags = {
-      Application = var.application
-      Environment = var.environment
-      Owner       = var.owner
-      CostCenter  = var.cost_center
+      version = "~> 6.0"
     }
   }
 }
