ce1487d5c4ee1cf090d92609aa7e52e0c81599b3
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-07-30 16:38:17 +0530

    Update data.tf
    Signed-off-by: deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Employee_App/data.tf
@@ -1,17 +1,21 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "otms-terraform-state-dev"
-    key    = "network/full.tfstate"
-    region = "us-east-1"
+# Fetch the existing VPC
+data "aws_vpc" "main_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
   }
 }
 
-data "aws_vpc" "main" {
-  id = data.terraform_remote_state.network.outputs.vpc_id
+# Fetch the existing ALB Security Group for Ingress rules
+data "aws_security_group" "alb_sg" {
+  filter {
+    name   = "tag:Name"
+    values = [var.alb_sg_name]
+  }
+  vpc_id = data.aws_vpc.main_vpc.id
 }
 
-data "aws_route53_zone" "private" {
-  name         = "otms.internal"
-  private_zone = true
+# Fetch the existing Target Group created in the Network Skeleton
+data "aws_lb_target_group" "employee_tg" {
+  name = var.target_group_name
 }
