03e23aae829e86772ea2e451c6a94775401ded87
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-07-30 08:51:18 +0530

    Update variables.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/variables.tf
@@ -1,29 +1,27 @@
-# --- GLOBAL VARIABLES ---
-variable "aws_region"  { type = string }
-variable "environment" { type = string }
-variable "application" { type = string }
-variable "cost_center" { type = string }
-variable "owner"       { type = string }
-
-# --- VPC VARIABLES ---
-variable "vpc_cidr" { type = string }
-variable "vpc_name" { type = string }
-
-# --- SUBNET VARIABLES ---
-variable "subnet_newbits" {
-  description = "Number of bits to add for subnetting"
-  type        = number
-  default     = 3
+variable "aws_region" {
+  type = string
 }
 
-variable "azs" {
-  description = "Availability zones suffixes to use"
-  type        = list(string)
-  default     = ["a", "b"]
+variable "public_subnet_name" {
+  type = string
 }
 
-# --- IGW VARIABLES ---
-variable "igw_name" {
-  description = "Name of the Internet Gateway"
-  type        = string
+variable "private_route_table_id" {
+  type = string
+}
+
+variable "nat_gateway_name" {
+  type = string
+}
+
+variable "eip_name" {
+  type = string
+}
+
+variable "destination_cidr" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
 }
