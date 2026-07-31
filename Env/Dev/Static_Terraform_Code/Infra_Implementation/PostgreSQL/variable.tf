9fc6b22222ef2dc9a1a1124527f13b4dbc661136
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-31 20:08:27 +0530

    Update variable.tf
    added variable description
    
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/PostgreSQL/variable.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/PostgreSQL/variable.tf
@@ -1,45 +1,54 @@
 variable "aws_region" {
-  type = string
+  description = "AWS region where the infrastructure resources will be deployed."
+  type        = string
 }
 
 variable "environment" {
-  type = string
+  description = "Deployment environment (e.g., dev, qa, staging, prod)."
+  type        = string
 }
 
 variable "application" {
-  type = string
+  description = "Name of the application for which the infrastructure is being provisioned."
+  type        = string
 }
 
 variable "owner" {
-  type = string
+  description = "Owner or team responsible for managing the infrastructure resources."
+  type        = string
 }
 
 variable "cost_center" {
-  type = string
+  description = "Cost center used for resource tagging and cost allocation."
+  type        = string
 }
 
 variable "vpc_name" {
-  type = string
+  description = "Name of the existing Virtual Private Cloud (VPC)."
+  type        = string
 }
 
 variable "database_subnet_name" {
-  type = string
+  description = "Name tag of the database subnet where the instance will be deployed."
+  type        = string
 }
 
 variable "key_name" {
-  type = string
+  description = "Name of the EC2 key pair used for SSH access."
+  type        = string
 }
 
 variable "instance_type" {
-  type = string
+  description = "EC2 instance type to launch (e.g., t3.micro, t3.small)."
+  type        = string
 }
 
 variable "ami_owner_id" {
+  description = "AWS Account ID of the AMI owner."
   type        = string
-  description = "AWS Account ID of the AMI owner"
 }
 
 variable "ami_name" {
+  description = "Name of the Amazon Machine Image (AMI) to be used for the EC2 instance."
   type        = string
-  description = "AMI name"
 }
