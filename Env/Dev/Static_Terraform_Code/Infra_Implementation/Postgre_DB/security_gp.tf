a2e6265bef1f0cf2e4779a4c14bd84bc636c5159
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-31 12:43:21 +0530

    Update security_gp.tf
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Postgre_DB/security_gp.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Postgre_DB/security_gp.tf
@@ -1,13 +1,3 @@
-# Lookup VPC
-data "aws_vpc" "main" {
-  tags = { Name = "${var.environment}-otms-vpc" }
-}
-
-# Lookup Attendance App Security Group
-data "aws_security_group" "attendance" {
-  tags = { Name = "${var.environment}-otms-attendance-sg" }
-}
-
 # PostgreSQL Security Group
 resource "aws_security_group" "postgresql" {
   name        = "${var.environment}-otms-postgres-sg"
