4756de5ab39d4fafa35ce51f8ab76d431426f82a
bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>
2026-07-31 07:42:05 +0530

    Update variables.tf
    Signed-off-by: bhawnadangarh <bhawna.dangarh.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App /variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App /variables.tf
@@ -1,54 +1,21 @@
 variable "aws_region" {
-  type    = string
-  default = "ap-south-1"
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region to deploy resources"
 }
 
 variable "environment" {
-  type    = string
-  default = "dev"
+  type        = string
+  default     = "dev"
+  description = "Deployment environment name"
 }
 
-variable "tag_application" {
-  type    = string
-  default = "Attendance-App"
-}
-
-variable "tag_owner" {
-  type    = string
-  default = "Bhawna"
-}
-
-variable "tag_costcenter" {
-  type    = string
-  default = "Dev-Cost-Center"
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-
-variable "ami_id" {
-  type    = string
-  default = "ami-0f5ee92e2d63afc18"
-}
-
-variable "key_name" {
-  type    = string
-  default = "sprint_3.1"
-}
-
-variable "min_size" {
-  type    = number
-  default = 1
-}
-
-variable "max_size" {
-  type    = number
-  default = 3
-}
-
-variable "desired_capacity" {
-  type    = number
-  default = 1
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags for all resources"
+  default = {
+    Application = "otms"
+    Owner       = "Infra-Titans"
+    CostCenter  = "Snaatak"
+  }
 }
