bd0b1236e704890f28bfbe8c122d2c8b3e3ef185
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-07-31 20:02:32 +0530

    Update vpc_variables.tf
    added description
    
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/vpc_variables.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/vpc_variables.tf
@@ -1,2 +1,9 @@
-variable "vpc_cidr" { type = string }
-variable "vpc_name" { type = string }
+variable "vpc_cidr" {
+  description = "CIDR block to be assigned to the Virtual Private Cloud (VPC)."
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the Virtual Private Cloud (VPC)."
+  type        = string
+}
