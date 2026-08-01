5e4851afbf98513f3c35b9f729b2fc45f056baa4
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-01 21:59:08 +0530

    Create data.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

---
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/data.tf
@@ -0,0 +1,68 @@
+
+data "aws_vpc" "network_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.network_vpc.id]
+  }
+
+  tags = {
+    Tier = "public"
+  }
+}
+
+data "aws_subnets" "backend_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.network_vpc.id]
+  }
+
+  tags = {
+    Tier = "backend"
+  }
+}
+
+data "aws_lb" "existing_alb" {
+  name = "${var.environment}-${var.application}-alb"
+}
+
+data "aws_lb_listener" "app_listener" {
+  load_balancer_arn = data.aws_lb.existing_alb.arn
+  port              = 443
+}
+
+data "aws_lb_listener_rule" "notification_routing" {
+  listener_arn = data.aws_lb_listener.app_listener.arn
+  priority     = 40
+}
+
+data "aws_lb_target_group" "notification_tg" {
+  name = "${var.environment}-${var.application}-notification-tg"
+}
+
+data "aws_security_group" "alb_sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["*alb*"]
+  }
+}
+
+data "aws_key_pair" "existing_key" {
+  key_name = var.key_name
+}
+
+data "aws_ami" "notification_app" {
+  most_recent = true
+  owners      = [var.ami_owner_id]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+}
