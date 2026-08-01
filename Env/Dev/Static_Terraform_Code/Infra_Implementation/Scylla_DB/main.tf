1442ab1e573ed4a080fbc16a1fda3a89c541ae21
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-01 22:38:56 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Scylla_DB/main.tf
@@ -7,44 +7,6 @@ locals {
   }
 }
 
-data "aws_vpc" "network_vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name]
-  }
-}
-
-data "aws_subnet" "database_subnet" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.network_vpc.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = [var.database_subnet_name]
-  }
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  owners = [var.ami_owner_id]
-
-  filter {
-    name   = "name"
-    values = [var.ami_name]
-  }
-}
-
-data "aws_key_pair" "existing_key" {
-  key_name = var.key_name
-}
-
-data "aws_iam_instance_profile" "ssm" {
-  name = var.ssm_instance_profile
-}
-
 resource "aws_security_group" "scylla_sg" {
   name        = "${var.environment}-${var.application}-scylla-sg"
   description = "Security Group for ScyllaDB EC2"
