1971eb3ac3a009bce40bb50a7cd9a32baef1cb2c
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-01 22:00:15 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/main.tf
@@ -1,34 +1,3 @@
-data "aws_vpc" "this" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name]
-  }
-}
-
-data "aws_subnet" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.this.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = [var.public_subnet_name]
-  }
-}
-
-data "aws_subnet" "backend" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.this.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = [var.backend_subnet_name]
-  }
-}
-
 resource "aws_internet_gateway" "this" {
   vpc_id = data.aws_vpc.this.id
 
