9e64bbfcc824cd11e8d881fd4cf679a28d046b82
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-01 21:58:03 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/main.tf
@@ -8,69 +8,6 @@ locals {
   }
 }
 
-data "aws_vpc" "network_vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name]
-  }
-}
-
-data "aws_subnets" "public_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.network_vpc.id]
-  }
-
-  tags = {
-    Tier = "public"
-  }
-}
-
-data "aws_subnets" "backend_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.network_vpc.id]
-  }
-
-  tags = {
-    Tier = "backend"
-  }
-}
-
-data "aws_lb" "existing_alb" {
-  name = "${var.environment}-${var.application}-alb"
-}
-
-data "aws_lb_listener" "app_listener" {
-  load_balancer_arn = data.aws_lb.existing_alb.arn
-  port              = 443
-}
-
-data "aws_lb_target_group" "notification_tg" {
-  name = "${var.environment}-${var.application}-notification-tg"
-}
-
-data "aws_security_group" "alb_sg" {
-  filter {
-    name   = "tag:Name"
-    values = ["*alb*"]
-  }
-}
-
-data "aws_key_pair" "existing_key" {
-  key_name = var.key_name
-}
-
-data "aws_ami" "notification_app" {
-  most_recent = true
-  owners      = [var.ami_owner_id]
-
-  filter {
-    name   = "name"
-    values = [var.ami_name]
-  }
-}
-
 resource "aws_security_group" "notification_sg" {
   name        = "${var.environment}-${var.application}-notification-sg"
   description = "Security group for notification backend instances"
