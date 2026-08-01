80d2c16f056fd880a00033bdd5d479d62a966446
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-01 21:51:57 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Notification_App/main.tf
@@ -1,17 +1,3 @@
-# Lookup Subnet
-data "aws_subnet" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet_name]
-  }
-}
-
-# Lookup IAM Instance Profile
-data "aws_iam_instance_profile" "selected" {
-  name = var.iam_instance_profile
-}
-
-# EC2 Instance
 resource "aws_instance" "this" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
