36eeddaa94731554873f78ed9b8ebee9a107b254
VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>
2026-08-02 12:07:39 +0530

    Update backend.tf
    Signed-off-by: VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
     key          = "network/vpc.tfstate"       
-    region       = "ap-south-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
