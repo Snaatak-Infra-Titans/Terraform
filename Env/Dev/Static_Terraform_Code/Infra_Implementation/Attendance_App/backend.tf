574ea82a658c9ef29f49d5c62a4590b0747fe7ab
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-08-06 14:17:24 +0530

    Update backend.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "otms-terraform-state-dev-788572613316"
-    key    = "env/dev/application/attendance/terraform.tfstate"
-    region = "us-east-1"
+    bucket       = "otms-terraform-state-dev"
+    key          = "attendance/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
