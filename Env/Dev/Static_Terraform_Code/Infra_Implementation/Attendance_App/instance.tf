df7a19a1c1f53c7cfdea9696669cf06bdcb5dcf2
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-08-06 13:57:03 +0530

    Update instance.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/instance.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/instance.tf
@@ -1,13 +1,29 @@
 resource "aws_instance" "attendance_api" {
   ami           = var.ami_id
-  instance_type = "t3.small"
+  instance_type = var.instance_type
 
-  subnet_id              = var.private_subnet_id
-  vpc_security_group_ids = [aws_security_group.attendance_api_sg.id]
+  subnet_id = data.aws_subnet.attendance.id
 
-  iam_instance_profile = aws_iam_instance_profile.attendance_ssm_profile.name
+  vpc_security_group_ids = [
+    data.aws_security_group.attendance_api.id
+  ]
+
+  iam_instance_profile = data.aws_iam_instance_profile.attendance_ssm.name
+
+  associate_public_ip_address = false
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
 
   tags = {
-    Name = "dev-attendance-api"
+    Name        = "${var.environment}-${var.application}-attendance-api"
+    Application = var.application
+    Environment = var.environment
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+    Service     = "attendance-api"
+    ManagedBy   = "Terraform"
   }
 }
