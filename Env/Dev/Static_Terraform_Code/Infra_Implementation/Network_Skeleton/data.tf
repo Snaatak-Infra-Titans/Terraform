199533d4d7703e50151c7831892320c0a4d83152
VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>
2026-08-06 19:21:04 +0530

    Update data.tf
    Signed-off-by: VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/data.tf
@@ -1,23 +1,9 @@
-############################################
-# Existing VPC
-############################################
-
 data "aws_vpc" "main" {
   id = var.vpc_id
 }
-
-############################################
-# Existing Public Subnet A
-############################################
-
 data "aws_subnet" "public_a" {
   id = var.public_subnet_a_id
 }
-
-############################################
-# Existing Public Subnet B
-############################################
-
 data "aws_subnet" "public_b" {
   id = var.public_subnet_b_id
 }
