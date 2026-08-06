0b1a02fad1a2f83ba2c127121c1c739cc860c9ed
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-08-06 14:55:44 +0530

    Delete Env/Dev/Static_Terraform_Code/Infra_Implementation/PostgreSQL/updated directory
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/PostgreSQL/updated/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the PostgreSQL EC2 instance."
-  value       = aws_instance.postgresql.id
-}
-
-output "private_ip" {
-  description = "Private IP address of the PostgreSQL EC2 instance."
-  value       = aws_instance.postgresql.private_ip
-}
