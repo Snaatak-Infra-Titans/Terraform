70431ae28f3640821cd39bda64ff702968e65649
Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>
2026-08-06 14:34:51 +0530

    Update data.tf
    Signed-off-by: Saransh23-snaatak <saransh.rai.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Attendance_App/data.tf
@@ -1,3 +1,4 @@
+# Fetch existing VPC using its Name tag.
 data "aws_vpc" "network_vpc" {
   filter {
     name   = "tag:Name"
@@ -5,65 +6,69 @@ data "aws_vpc" "network_vpc" {
   }
 }
 
+# Fetch backend/private subnets from the selected VPC.
 data "aws_subnets" "backend_subnets" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.network_vpc.id]
   }
-  tags = {
-    Tier = "backend"
+
+  filter {
+    name   = "tag:Tier"
+    values = ["backend"]
   }
 }
 
+# Fetch public subnets from the selected VPC.
 data "aws_subnets" "public_subnets" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.network_vpc.id]
   }
 
-  tags = {
-    Tier = "public"
+  filter {
+    name   = "tag:Tier"
+    values = ["public"]
   }
 }
 
-# Fetching the existing ALB from Network Skeleton
+# Fetch existing ALB.
 data "aws_lb" "existing_alb" {
   name = "${var.environment}-${var.application}-alb"
 }
 
-# Fetching the HTTPS Listener (Port 443) for routing rules
+# Fetch HTTPS listener from the existing ALB.
 data "aws_lb_listener" "app_listener" {
   load_balancer_arn = data.aws_lb.existing_alb.arn
   port              = 443
 }
 
-data "aws_security_group" "alb_sg" {
-  filter {
-    name   = "tag:Name"
-    values = ["*alb*"]
-  }
-}
-
-/*
-data "aws_security_group" "bastion_sg" {
-  filter {
-    name   = "tag:Name"
-    values = ["*bastion*"]
-  }
-}
-*/
-
+# Fetch existing EC2 key pair.
 data "aws_key_pair" "existing_key" {
   key_name = var.key_name
 }
 
+# Fetch existing Attendance API AMI.
 data "aws_ami" "attendance_app" {
+  owners = ["self"]
+
   filter {
     name   = "image-id"
     values = [var.ami_id]
   }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 }
 
+# Fetch existing Attendance API target group.
 data "aws_lb_target_group" "attendance_tg" {
   name = "${var.environment}-${var.application}-attendance-tg"
+}
+
+# Fetch existing IAM instance profile.
+data "aws_iam_instance_profile" "attendance_ssm" {
+  name = var.ssm_instance_profile
 }
