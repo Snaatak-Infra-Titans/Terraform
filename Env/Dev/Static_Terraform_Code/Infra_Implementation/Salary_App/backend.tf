06fad97834a65cf89e01309cb717583464883025
VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>
2026-08-07 23:36:01 +0530

    Update backend.tf
    Signed-off-by: VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "otms-terraform-state-dev"
-    key          = "salary/lt.tfstate"
+    key          = "salary/asg.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
