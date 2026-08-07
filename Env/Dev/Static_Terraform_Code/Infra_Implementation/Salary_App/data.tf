f76d127044053b438383572081a163c114b92fad
VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>
2026-08-07 21:58:20 +0530

    Update data.tf
    Signed-off-by: VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/data.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/data.tf
@@ -5,38 +5,6 @@ data "aws_vpc" "network_vpc" {
   }
 }
 
-data "aws_subnets" "backend_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.network_vpc.id]
-  }
-  tags = {
-    Tier = "backend"
-  }
-}
-
-data "aws_subnets" "public_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.network_vpc.id]
-  }
-
-  tags = {
-    Tier = "public"
-  }
-}
-
-# Fetching the existing ALB from Network Skeleton
-data "aws_lb" "existing_alb" {
-  name = "${var.environment}-${var.application}-alb"
-}
-
-# Fetching the HTTPS Listener (Port 443) for routing rules
-data "aws_lb_listener" "app_listener" {
-  load_balancer_arn = data.aws_lb.existing_alb.arn
-  port              = 443
-}
-
 data "aws_security_group" "alb_sg" {
   filter {
     name   = "tag:Name"
@@ -44,14 +12,12 @@ data "aws_security_group" "alb_sg" {
   }
 }
 
-/*
-data "aws_security_group" "bastion_sg" {
+data "aws_security_group" "salary_sg" {
   filter {
     name   = "tag:Name"
-    values = ["*bastion*"]
+    values = ["dev-otms-salary-sg"]
   }
 }
-*/
 
 data "aws_key_pair" "existing_key" {
   key_name = var.key_name
