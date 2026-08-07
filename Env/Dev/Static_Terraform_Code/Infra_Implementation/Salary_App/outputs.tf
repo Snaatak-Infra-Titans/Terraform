b37f58baae69e06429525e04caad7d0a46ded3f8
VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>
2026-08-07 23:39:49 +0530

    Update outputs.tf
    Signed-off-by: VarshaTripathi905 <versha.tripathi.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Salary_App/outputs.tf
@@ -1,14 +1,29 @@
-output "notification_asg_id" {
-  description = "The ID of the notification Auto Scaling Group"
-  value       = aws_autoscaling_group.notification_asg.id
+output "salary_asg_id" {
+  description = "The ID of the Salary Auto Scaling Group"
+  value       = aws_autoscaling_group.salary_asg.id
 }
 
-output "notification_asg_name" {
-  description = "The name of the notification Auto Scaling Group"
-  value       = aws_autoscaling_group.notification_asg.name
+output "salary_asg_name" {
+  description = "The name of the Salary Auto Scaling Group"
+  value       = aws_autoscaling_group.salary_asg.name
 }
 
-output "notification_asg_arn" {
-  description = "The ARN of the notification Auto Scaling Group"
-  value       = aws_autoscaling_group.notification_asg.arn
+output "salary_asg_arn" {
+  description = "The ARN of the Salary Auto Scaling Group"
+  value       = aws_autoscaling_group.salary_asg.arn
+}
+
+output "salary_asg_desired_capacity" {
+  description = "The desired capacity of the Salary Auto Scaling Group"
+  value       = aws_autoscaling_group.salary_asg.desired_capacity
+}
+
+output "salary_asg_min_size" {
+  description = "The minimum size of the Salary Auto Scaling Group"
+  value       = aws_autoscaling_group.salary_asg.min_size
+}
+
+output "salary_asg_max_size" {
+  description = "The maximum size of the Salary Auto Scaling Group"
+  value       = aws_autoscaling_group.salary_asg.max_size
 }
