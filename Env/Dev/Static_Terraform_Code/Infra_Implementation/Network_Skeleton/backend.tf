e6b6c47a512cd00ac3f87c72dc30b4033ed48809
Pawan2031 <pawan.tiwari.snaatak@mygurukulam.co>
2026-08-14 09:12:19 +0530

    Change S3 bucket name for Terraform backend
    Signed-off-by: Pawan2031 <pawan.tiwari.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "otms-terraform-state-dev"
+    bucket       = "dev-otms-terraform-state"
     key          = "network/subnets.tfstate"
     region       = "us-east-1"
     encrypt      = true
