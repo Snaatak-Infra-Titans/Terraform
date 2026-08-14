5bbf7364264e639f49b082752d016937047973d5
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-14 20:13:26 +0530

    Refactor outputs in outputs.tf for Notification module
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Wrapper/Notification/outputs.tf
+++ Env/Dev/Wrapper/Notification/outputs.tf
@@ -1,24 +1,29 @@
-output "autoscaling_group_name" {
-  description = "Name of the Notification Auto Scaling Group"
-  value       = module.notification.autoscaling_group_name
+output "target_group_arn" {
+  description = "ARN of the Notification Auto Scaling target group"
+
+  value = module.notification.target_group_arn
 }
 
 output "launch_template_id" {
-  description = "ID of the Notification Launch Template"
-  value       = module.notification.launch_template_id
+  description = "ID of the Notification launch template"
+
+  value = module.notification.launch_template_id
 }
 
-output "target_group_arn" {
-  description = "ARN of the Notification Target Group"
-  value       = module.notification.target_group_arn
-}
+output "autoscaling_group_name" {
+  description = "Name of the Notification Auto Scaling Group"
 
-output "listener_rule_arn" {
-  description = "ARN of the Notification ALB listener rule"
-  value       = module.notification.listener_rule_arn
+  value = module.notification.autoscaling_group_name
 }
 
 output "autoscaling_policy_name" {
   description = "Name of the Notification Auto Scaling policy"
-  value       = module.notification.autoscaling_policy_name
+
+  value = module.notification.autoscaling_policy_name
+}
+
+output "listener_rule_arn" {
+  description = "ARN of the Notification ALB listener rule"
+
+  value = module.notification.listener_rule_arn
 }
