6c684c84d21a5ad0621f3f6e95683b2fe5e5db0b
deep-snaatak <deepak.nagar.snaatak@mygurukulam.co>
2026-08-17 12:37:11 +0530

    SCRUM-499 remove Terraform implementation from CD branch

--- Modules/Network_Skeleton/examples/complete/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "dev-otms-terraform-state-352742379893"
-    key          = "network-skeleton-module/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
