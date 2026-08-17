74934e2b1cc8a47bd6b1f3e03d1e48d750d12cd5
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-08-17 22:22:22 +0530

    Update outputs.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Modules/Auto_Scaling/outputs.tf
+++ Modules/Auto_Scaling/outputs.tf
@@ -1,29 +1,54 @@
+############################################
+# Target Group Outputs
+############################################
+
 output "target_group_arn" {
   description = "ARN of the Auto Scaling target group"
-
-  value = aws_lb_target_group.this.arn
+  value       = aws_lb_target_group.this.arn
 }
 
-output "launch_template_id" {
-  description = "ID of the Auto Scaling launch template"
-
-  value = aws_launch_template.this.id
+output "target_group_name" {
+  description = "Name of the Auto Scaling target group"
+  value       = aws_lb_target_group.this.name
 }
 
-output "autoscaling_group_name" {
-  description = "Name of the Auto Scaling Group"
-
-  value = aws_autoscaling_group.this.name
-}
-
-output "autoscaling_policy_name" {
-  description = "Name of the Auto Scaling policy"
-
-  value = aws_autoscaling_policy.this.name
-}
+############################################
+# Listener Rule Output
+############################################
 
 output "listener_rule_arn" {
   description = "ARN of the ALB listener rule"
+  value       = aws_lb_listener_rule.this.arn
+}
 
-  value = aws_lb_listener_rule.this.arn
+############################################
+# Launch Template Outputs
+############################################
+
+output "launch_template_id" {
+  description = "ID of the Launch Template"
+  value       = aws_launch_template.this.id
+}
+
+output "launch_template_arn" {
+  description = "ARN of the Launch Template"
+  value       = aws_launch_template.this.arn
+}
+
+############################################
+# Auto Scaling Group Output
+############################################
+
+output "autoscaling_group_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.this.name
+}
+
+############################################
+# Auto Scaling Policy Output
+############################################
+
+output "autoscaling_policy_name" {
+  description = "Name of the Auto Scaling policy"
+  value       = aws_autoscaling_policy.this.name
 }
