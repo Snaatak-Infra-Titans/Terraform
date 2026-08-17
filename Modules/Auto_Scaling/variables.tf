24fcebd5bdb0c978122968b2e0a0329e74277f04
AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>
2026-08-17 22:22:06 +0530

    Update variables.tf
    Signed-off-by: AnkitaGS-18 <ankita.singh.snaatak@mygurukulam.co>

--- Modules/Auto_Scaling/variables.tf
+++ Modules/Auto_Scaling/variables.tf
@@ -1,32 +1,45 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID where the target group is created"
-  type        = string
-}
+############################################
+# Common / Naming Variables
+############################################
 
 variable "environment" {
-  description = "Deployment environment"
+  description = "Deployment environment such as dev, qa, stage, or prod"
   type        = string
 }
 
 variable "application" {
-  description = "Application name"
+  description = "Application or service name"
   type        = string
 }
 
 variable "common_tags" {
-  description = "Common tags applied to AWS resources"
+  description = "Common tags applied to Auto Scaling resources"
   type        = map(string)
   default     = {}
 }
 
-# --------------------------------------------------
+############################################
+# Network Inputs
+############################################
+
+variable "vpc_id" {
+  description = "VPC ID where the target group will be created"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs where Auto Scaling instances will be launched"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security Group ID attached to Auto Scaling instances"
+  type        = string
+}
+
+############################################
 # Target Group
-# --------------------------------------------------
+############################################
 
 variable "application_port" {
   description = "Port on which the application listens"
@@ -34,21 +47,38 @@ variable "application_port" {
 }
 
 variable "target_group_protocol" {
-  description = "Protocol used by the target group"
+  description = "Protocol used between the ALB target group and application instances"
   type        = string
-  default     = "HTTPS"
+  default     = "HTTP"
 
   validation {
-    condition     = contains(["HTTP", "HTTPS"], var.target_group_protocol)
+    condition = contains(
+      ["HTTP", "HTTPS"],
+      var.target_group_protocol
+    )
+
     error_message = "target_group_protocol must be HTTP or HTTPS."
   }
 }
 
 variable "target_type" {
-  description = "Target type for the target group"
+  description = "Target type used by the ALB target group"
   type        = string
   default     = "instance"
+
+  validation {
+    condition = contains(
+      ["instance", "ip"],
+      var.target_type
+    )
+
+    error_message = "target_type must be instance or ip."
+  }
 }
+
+############################################
+# Target Group Health Check
+############################################
 
 variable "health_check_enabled" {
   description = "Whether target group health checks are enabled"
@@ -57,24 +87,28 @@ variable "health_check_enabled" {
 }
 
 variable "health_check_protocol" {
-  description = "Protocol used for target group health checks"
+  description = "Protocol used by target group health checks"
   type        = string
-  default     = "HTTPS"
+  default     = "HTTP"
 
   validation {
-    condition     = contains(["HTTP", "HTTPS"], var.health_check_protocol)
+    condition = contains(
+      ["HTTP", "HTTPS"],
+      var.health_check_protocol
+    )
+
     error_message = "health_check_protocol must be HTTP or HTTPS."
   }
 }
 
 variable "health_check_path" {
-  description = "Health check path"
+  description = "Application health check path"
   type        = string
   default     = "/"
 }
 
 variable "health_check_port" {
-  description = "Port used for health checks"
+  description = "Port used for target group health checks"
   type        = string
   default     = "traffic-port"
 }
@@ -92,48 +126,57 @@ variable "health_check_timeout" {
 }
 
 variable "healthy_threshold" {
-  description = "Number of consecutive successful health checks"
+  description = "Number of successful health checks required before considering a target healthy"
   type        = number
-  default     = 3
+  default     = 2
 }
 
 variable "unhealthy_threshold" {
-  description = "Number of consecutive failed health checks"
+  description = "Number of failed health checks required before considering a target unhealthy"
   type        = number
   default     = 3
 }
 
 variable "health_check_matcher" {
-  description = "Expected HTTP response codes"
+  description = "Expected HTTP response code or range for health checks"
   type        = string
-  default     = "200"
+  default     = "200-399"
 }
 
-# --------------------------------------------------
-# Listener Rule
-# --------------------------------------------------
+############################################
+# ALB Listener Rule
+############################################
 
 variable "listener_arn" {
-  description = "ARN of the existing ALB listener"
+  description = "ARN of the existing HTTPS ALB listener created by the Network Skeleton module"
   type        = string
 }
 
 variable "listener_rule_priority" {
-  description = "Priority of the ALB listener rule"
+  description = "Unique priority assigned to this listener rule"
   type        = number
+
+  validation {
+    condition = (
+      var.listener_rule_priority >= 1 &&
+      var.listener_rule_priority <= 50000
+    )
+
+    error_message = "listener_rule_priority must be between 1 and 50000."
+  }
 }
 
 variable "listener_rule_paths" {
-  description = "Path patterns used by the listener rule"
+  description = "Path patterns routed to this application's target group"
   type        = list(string)
 }
 
-# --------------------------------------------------
+############################################
 # Launch Template
-# --------------------------------------------------
+############################################
 
 variable "ami_id" {
-  description = "AMI ID used by the launch template"
+  description = "AMI ID used by the Launch Template"
   type        = string
 }
 
@@ -142,47 +185,51 @@ variable "instance_type" {
   type        = string
 }
 
-variable "security_group_id" {
-  description = "Security group ID attached to the instances"
-  type        = string
-}
-
 variable "iam_instance_profile_name" {
-  description = "IAM instance profile attached to the instances"
+  description = "Existing IAM instance profile attached to the Auto Scaling instances"
   type        = string
 }
 
-# --------------------------------------------------
+############################################
 # Auto Scaling Group
-# --------------------------------------------------
-
-variable "subnet_ids" {
-  description = "Subnet IDs used by the Auto Scaling Group"
-  type        = list(string)
-}
+############################################
 
 variable "desired_capacity" {
-  description = "Desired number of instances"
+  description = "Desired number of EC2 instances"
   type        = number
   default     = 1
 }
 
 variable "min_size" {
-  description = "Minimum number of instances"
+  description = "Minimum number of EC2 instances"
   type        = number
   default     = 1
 }
 
 variable "max_size" {
-  description = "Maximum number of instances"
+  description = "Maximum number of EC2 instances"
   type        = number
-  default     = 1
+  default     = 2
+
+  validation {
+    condition     = var.max_size >= 1
+    error_message = "max_size must be at least 1."
+  }
 }
 
 variable "asg_health_check_type" {
   description = "Health check type used by the Auto Scaling Group"
   type        = string
   default     = "ELB"
+
+  validation {
+    condition = contains(
+      ["EC2", "ELB"],
+      var.asg_health_check_type
+    )
+
+    error_message = "asg_health_check_type must be EC2 or ELB."
+  }
 }
 
 variable "asg_health_check_grace_period" {
@@ -191,18 +238,18 @@ variable "asg_health_check_grace_period" {
   default     = 300
 }
 
-# --------------------------------------------------
+############################################
 # Auto Scaling Policy
-# --------------------------------------------------
+############################################
 
 variable "scaling_metric_type" {
-  description = "Predefined metric used for target tracking"
+  description = "Predefined metric used by target tracking scaling"
   type        = string
   default     = "ASGAverageCPUUtilization"
 }
 
 variable "scaling_target_value" {
-  description = "Target value for the scaling policy"
+  description = "Target value maintained by the target tracking scaling policy"
   type        = number
   default     = 70
 }
