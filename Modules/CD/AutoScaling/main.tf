e3fff8497d8810ae50ef53c5c80c2055a8cf73ab
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-21 11:24:38 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Modules/CD/AutoScaling/main.tf
+++ Modules/CD/AutoScaling/main.tf
@@ -1,9 +1,12 @@
 module "autoscaling" {
   source = "git::https://github.com/Snaatak-Infra-Titans/Terraform.git//Modules/Auto_Scaling?ref=SCRUM-482-ankita"
-  listener_arn             = var.listener_arn
-  listener_rule_priority   = var.listener_rule_priority
-  listener_rule_paths      = var.listener_rule_paths
-  ami_id                   = var.ami_id
-  instance_type            = var.instance_type
+  environment               = var.environment
+  application               = var.application
+  vpc_id                    = var.vpc_id
+  subnet_ids                = var.subnet_ids
+  security_group_id         = var.security_group_id
+  application_port          = var.application_port
+  ami_id                    = var.ami_id
+  instance_type             = var.instance_type
   iam_instance_profile_name = var.iam_instance_profile_name
 }
