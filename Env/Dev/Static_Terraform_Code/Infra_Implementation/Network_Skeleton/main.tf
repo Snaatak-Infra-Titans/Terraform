fe8aecec904d621b323a7ca367e52dda564b411b
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-08-27 14:05:49 +0530

    Update main.tf
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/main.tf
+++ Env/Dev/Static_Terraform_Code/Infra_Implementation/Network_Skeleton/main.tf
@@ -1,22 +1,29 @@
-resource "aws_eip" "this" {
+locals {
+  name_prefix = "${var.environment}-${var.application}"
+
+  nat_eip_name     = "${local.name_prefix}-nat-eip"
+  nat_gateway_name = "${local.name_prefix}-nat-gateway"
+}
+
+resource "aws_eip" "nat_eip" {
   domain = "vpc"
 
   tags = merge(
     var.tags,
     {
-      Name = "${var.environment}-${var.application}-nat-eip"
+      Name = local.nat_eip_name
     }
   )
 }
 
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.this.id
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = data.aws_subnet.public.id
 
   tags = merge(
     var.tags,
     {
-      Name = "${var.environment}-${var.application}-nat-gateway"
+      Name = local.nat_gateway_name
     }
   )
 }
